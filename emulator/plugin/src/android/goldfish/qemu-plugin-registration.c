965b07bf31be4ff562d8c60f48c56b17aa85e6de
Erwin Jansen <jansene@google.com>
2024-05-08 19:05:42 -0700

    Update registration and distribution with Rutabaga
    This makes the rutabaga module available in Qemu, note that we do not
    yet enable it on windows due to some rust toolchain issues.
    
    Bug: 335734830
    Change-Id: Ifaf0d0462a5c9b4fa45a071cc7c477ba1035204c

--- emulator/plugin/src/android/goldfish/qemu-plugin-registration.c
+++ emulator/plugin/src/android/goldfish/qemu-plugin-registration.c
@@ -1,27 +1,29 @@
+
+// clang-format off
+// IWYU pragma: begin_keep
 #include "qemu/osdep.h"
 #include "qemu/module.h"
+// IWYU pragma: end_keep
+// clang-format on
+
 const QemuModinfo qemu_modinfo[] = {
     {
-        /* hw-display-virtio-gpu.modinfo */
-        .name = "hw-display-virtio-gpu",
-        .objs = ((const char *[]){"virtio-gpu-base", "virtio-gpu-device",
-                                  "vhost-user-gpu", NULL}),
+        /* hw-display-virtio-gpu-rutabaga.modinfo */
+        .name = "hw-display-virtio-gpu-rutabaga",
+        .objs = ((const char *[]){"virtio-gpu-rutabaga-device", NULL}),
+        .deps = ((const char *[]){"hw-display-virtio-gpu", NULL}),
     },
     {
-        /* hw-display-virtio-gpu-pci.modinfo */
-        .name = "hw-display-virtio-gpu-pci",
-        .objs = ((const char *[]){"virtio-gpu-pci-base", "virtio-gpu-pci",
-                                  "vhost-user-gpu-pci", NULL}),
+        /* hw-display-virtio-gpu-pci-rutabaga.modinfo */
+        .name = "hw-display-virtio-gpu-pci-rutabaga",
+        .objs = ((const char *[]){"virtio-gpu-rutabaga-pci", NULL}),
+        .deps = ((const char *[]){"hw-display-virtio-gpu-pci", NULL}),
     },
     {
-        /* hw-display-virtio-vga.modinfo */
-        .name = "hw-display-virtio-vga",
-        .objs = ((const char *[]){"virtio-vga-base", "virtio-vga",
-                                  "vhost-user-vga", NULL}),
-    },
-    {
-        /* hw-display-virtio-vga-gl.modinfo */
-        .name = "hw-display-virtio-vga-gl",
+        /* hw-display-virtio-vga-rutabaga.modinfo */
+        .name = "hw-display-virtio-vga-rutabaga",
+        .objs = ((const char *[]){"virtio-vga-rutabaga", NULL}),
+        .deps = ((const char *[]){"hw-display-virtio-vga", NULL}),
     },
     {
         /* audio-pa.modinfo */
