10641bff9b00cf1cc5d668f7b36f9ccad38a7450
Erwin Jansen <jansene@google.com>
2024-05-17 10:45:29 -0700

    Initial port of gRPC services
    This brings in the framework for the grpc services. This is a partial
    port from emu-master-dev:
    
    - AdbService
    - Security support
    - Basic interceptor support
    - Configuration and launch support
    
    Includes a series of tests to validate the behavior.
    
    Bug: 331190608
    Change-Id: I1d20bc70e8363edef908c93bc8e6fd0e52f1e3ef

---
+++ emulator/android-grpc/interceptors/src/android/emulation/control/interceptor/IdleInterceptor.cpp
@@ -0,0 +1,86 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "android/emulation/control/interceptor/IdleInterceptor.h"
+
+#include <cstdint>
+#include <functional>
+#include <ratio>
+
+#include "aemu/base/Log.h"
+#include "aemu/base/async/ThreadLooper.h"
+#include "aemu/base/process/Process.h"
+#include "android/base/system/System.h"
+
+namespace android {
+namespace control {
+namespace interceptor {
+
+using namespace grpc::experimental;
+
+IdleInterceptor::IdleInterceptor(std::chrono::seconds timeout,
+                                 std::atomic<uint64_t> *terminationUnixTime,
+                                 std::atomic<uint64_t> *activeRequests)
+    : mTimeout(timeout), mTerminationUnixTime(terminationUnixTime),
+      mActiveRequests(activeRequests) {}
+
+IdleInterceptor::~IdleInterceptor() {
+  auto idleTime = System::get()->getUnixTime() + mTimeout.count();
+  mTerminationUnixTime->store(idleTime);
+  mActiveRequests->fetch_sub(1);
+}
+
+void IdleInterceptor::Intercept(InterceptorBatchMethods *methods) {
+  methods->Proceed();
+}
+
+IdleInterceptorFactory::IdleInterceptorFactory(std::chrono::seconds timeout)
+    : mTimeout(timeout),
+      mTerminationUnixTime(System::get()->getUnixTime() + timeout.count()),
+      mTimeoutChecker(
+          android::base::ThreadLooper::get(),
+          [=]() { return checkIdleTimeout(); },
+          std::chrono::milliseconds(mTimeout).count()) {
+  mTimeoutChecker.start();
+}
+
+Interceptor *
+IdleInterceptorFactory::CreateServerInterceptor(ServerRpcInfo *info) {
+  mActiveRequests++;
+  return new IdleInterceptor(mTimeout, &mTerminationUnixTime, &mActiveRequests);
+}
+
+bool IdleInterceptorFactory::checkIdleTimeout() {
+  auto epoch = System::get()->getUnixTime();
+  if (mActiveRequests > 0 || epoch < mTerminationUnixTime)
+    return true;
+
+  LOG(WARNING) << "Idled to long, shutting down. " << epoch << " > "
+               << mTerminationUnixTime;
+  if (mShutdownAttempt == 0) {
+    LOG(WARNING) << "Trying nicely is not yet implemented..";
+  } else {
+    LOG(INFO) << "Terminating the emulator.";
+    auto me = android::base::Process::me();
+    if (me) {
+      me->terminate();
+    }
+  }
+
+  mShutdownAttempt++;
+  return true;
+}
+
+} // namespace interceptor
+} // namespace control
+} // namespace android
---
+++ emulator/android-grpc/services/emulator-controller/server/src/android/emulation/control/EmulatorService.cpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "android/emulation/control/EmulatorService.h"
+#include "hardware/generic/goldfish/emulator/android-grpc/services/emulator-controller/proto/emulator_controller.grpc.pb.h"
+
+namespace android {
+namespace emulation {
+namespace control {
+
+// Logic and data behind the server's behavior.
+class EmulatorControllerImpl final : public EmulatorController::Service {
+public:
+  EmulatorControllerImpl() = default;
+};
+
+grpc::Service *getEmulatorController() { return new EmulatorControllerImpl(); }
+
+} // namespace control
+} // namespace emulation
+} // namespace android
