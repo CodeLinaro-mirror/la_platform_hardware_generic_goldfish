2206b52aeed9f58fe46ad03a29782da99469931b
Roman Kiryanov <rkir@google.com>
2024-07-18 07:16:18 +0000

    Some refactoring in the broadcasting library
    * callback lifetimes are fixed
    * API improved
    * documentation added
    
    Bug: 353756694
    Test: broadcasting_unittests
    Change-Id: I159262d478c515aa7b15953db98655e48f86fcae
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- emulator/libs/broadcasting/broadcasting_unittests.cpp
+++ emulator/libs/broadcasting/broadcasting_unittests.cpp
@@ -14,76 +14,70 @@
 
 #include "goldfish/broadcasting.h"
 
-using Topic = goldfish::broadcasting::Topic;
-using IntegerTopic = goldfish::broadcasting::TopicT<int>;
-
 namespace {
+using goldfish::broadcasting::Ticket;
+using IntegerTopic = goldfish::broadcasting::Topic<int>;
+
 struct MySubscriber {
     bool notify(const int x) {
         value = x;
-        return true;
+        return wantMoreBroadcasts;
     }
 
-    static bool notifyS(void *that, const int x) {
-        return static_cast<MySubscriber *>(that)->notify(x);
-    }
-
+    Ticket ticket;
     int value = 0;
-    Topic::Ticket ticket = Topic::kEmptyTicket;
+    bool wantMoreBroadcasts = true;
 };
+
+using MySubscriberPtr = std::shared_ptr<MySubscriber>;
 }  // namespace
 
-TEST(broadcasting, subscribe) {
-    std::vector<MySubscriber> subscribers(5);
-
-    {
-        IntegerTopic integerTopic;
-        Topic::Ticket t = 0;
-        for (MySubscriber &s : subscribers) {
-            integerTopic.subscribe(&s, &MySubscriber::notifyS, &s.ticket);
-            EXPECT_EQ(s.ticket, ++t);
-        }
-    }
-
-    // `integerTopic` is no longer valid, the tickets must be empty
-    for (const MySubscriber &s : subscribers) {
-        EXPECT_EQ(s.ticket, Topic::kEmptyTicket);
-    }
-}
-
-TEST(broadcasting, broadcast) {
-    std::vector<MySubscriber> subscribers(5);
+TEST(broadcasting, example) {
     IntegerTopic integerTopic;
-
-    for (MySubscriber &s : subscribers) {
-        integerTopic.subscribe(&s, &MySubscriber::notifyS, &s.ticket);
-        EXPECT_NE(s.ticket, Topic::kEmptyTicket);
+    std::vector<MySubscriberPtr> subscribers(5);
+    for (MySubscriberPtr &s : subscribers) {
+        s = std::make_shared<MySubscriber>();
     }
 
     integerTopic.broadcast(42);
-    for (const MySubscriber &s : subscribers) {
-        EXPECT_EQ(s.value, 42);
+    for (MySubscriberPtr &s : subscribers) {
+        EXPECT_EQ(s->value, 0);  // not subscribed yet
+        s->ticket = integerTopic.subscribe(s, &MySubscriber::notify);
+        EXPECT_FALSE(s->ticket.empty());  // now subscribed
     }
+
+    integerTopic.broadcast(42);
+    for (const MySubscriberPtr &s : subscribers) {
+        EXPECT_EQ(s->value, 42);
+    }
+
+    // unsubscribe two
+    integerTopic.unsubscribe(&subscribers[0]->ticket);
+    EXPECT_TRUE(subscribers[0]->ticket.empty());
+    integerTopic.unsubscribe(&subscribers[2]->ticket);
+    EXPECT_TRUE(subscribers[2]->ticket.empty());
 
     integerTopic.broadcast(77);
-    for (const MySubscriber &s : subscribers) {
-        EXPECT_EQ(s.value, 77);
-    }
-}
+    EXPECT_EQ(subscribers[0]->value, 42);  // unsubscribed above
+    EXPECT_EQ(subscribers[1]->value, 77);
+    EXPECT_EQ(subscribers[2]->value, 42);  // unsubscribed above
+    EXPECT_EQ(subscribers[3]->value, 77);
+    EXPECT_EQ(subscribers[4]->value, 77);
 
-TEST(broadcasting, unsubscribe) {
-    std::vector<MySubscriber> subscribers(5);
-    IntegerTopic integerTopic;
+    subscribers[3]->wantMoreBroadcasts = false;
+    subscribers[4]->wantMoreBroadcasts = false;
 
-    for (MySubscriber &s : subscribers) {
-        integerTopic.subscribe(&s, &MySubscriber::notifyS, &s.ticket);
-        EXPECT_NE(s.ticket, Topic::kEmptyTicket);
-    }
+    integerTopic.broadcast(15);
+    EXPECT_EQ(subscribers[0]->value, 42);
+    EXPECT_EQ(subscribers[1]->value, 15);
+    EXPECT_EQ(subscribers[2]->value, 42);
+    EXPECT_EQ(subscribers[3]->value, 15);  // this broadcast is still received
+    EXPECT_EQ(subscribers[4]->value, 15);  // this broadcast is still received
 
-    EXPECT_EQ(subscribers[2].ticket, 3);
-    EXPECT_EQ(subscribers[4].ticket, 5);
-
-    integerTopic.unsubscribe(&subscribers[2].ticket);
-
-    EXPECT_EQ(subscribers[4].ticket, 3);
+    integerTopic.broadcast(99);
+    EXPECT_EQ(subscribers[0]->value, 42);
+    EXPECT_EQ(subscribers[1]->value, 99);
+    EXPECT_EQ(subscribers[2]->value, 42);
+    EXPECT_EQ(subscribers[3]->value, 15);  // unsubscribed, see `wantMoreBroadcasts` above
+    EXPECT_EQ(subscribers[4]->value, 15);  // unsubscribed, see `wantMoreBroadcasts` above
 }
--- emulator/libs/broadcasting/include/goldfish/broadcasting.h
+++ emulator/libs/broadcasting/include/goldfish/broadcasting.h
@@ -11,93 +11,160 @@
 */
 
 #pragma once
+#include <functional>
 #include <mutex>
-#include <vector>
+#include <unordered_map>
 #include <utility>
 
 namespace goldfish {
 namespace broadcasting {
 
-struct Topic {
-    using Ticket = unsigned;
-    using UpdateTicketFn = void (*)(void *, Ticket newTicket);
-    static constexpr Ticket kEmptyTicket = 0;
+/* See broadcasting_unittests.cpp for examples.
+ *
+ * `Topic<Args...>` a class to send broadcasts that take arguments
+ * `Args...` to subscribers. Please note that `Args...` can be
+ * `void` (see `Topic<void>` below) which means "no arguments".
 
-    ~Topic();
+ * Callback:
+ * 1. std::function<bool(Args...)>, it is taken by value and stored
+ *    inside the `Topic` instance until you unsubscribe by calling
+ *    `Topic::unsubscribe` explicitly or by returning `false` from
+ *    the callback. It is up to you how to manage the lifetimes of
+ *    objects which you function depends on.
+ * 2. The `Topic` class provides the `subscribe` call
+ *    (see `Topic::subscribe`) to build the function above from
+ *    `const std::shared_ptr<T> &` and a pointer to T's method.
+ *    The callback stores a weak pointer to `T` and calling
+ *    `Topic::unsubscribe` is optional in this case: the callback
+ *     will return `false` once `T` is destroyed.
+ *
+ * Subscribing:
+ * 1. Gives you a ticket to unsubscribe.
+ * 2. Subscribing multiple times with the same arguments will create
+ *    multiple subscriptions with different tickets.
+ *
+ * Explicit unsubsribing (`Topic::unsubscribe`):
+ * 1. Passing an empty (see `bool Ticket::empty() const`) ticket has
+ *    no effect.
+ * 2. optional, if your callback handles lifetimes of its dependencies.
+ * 3. calling `Topic::unsubscribe` with the same `Ticket` multiple
+ *    times is undefined behavior:`Ticket` values could be reused,
+ *    which means you would be unsubscribing someone else), see
+ *   `Topic::unsubscribe(Ticket *ticket)` to clear the ticket value.
+ */
 
-    void unsubscribe(Ticket *ticket);
-
-    Topic(const Topic &) = delete;
-    Topic(Topic &&) = delete;
-    Topic& operator=(const Topic &) = delete;
-    Topic& operator=(Topic &&) = delete;
-
-protected:
-    Topic() = default;
-
-    struct Subscription {
-        Ticket *pTicket;
-        void *object;
-        void *notifyFn;
-    };
-
-    void subscribeImpl(void *object, void *notifyFn, Ticket *pTicket);
-    void unsubscribeImpl(size_t i, size_t size);
-
-    std::vector<Subscription> mSubscriptions;
-    mutable std::mutex mMutex;
+struct Ticket {
+    using value_t = unsigned;
+    static constexpr value_t kEmpty = 0;
+    value_t value = kEmpty;
+    bool empty() const { return value == kEmpty; }
 };
 
-template <class... Args> struct TopicT : public Topic {
-    using NotifyFn = bool (*)(void *, Args...);
+template <class Callback> struct TopicBaseTpl {
+    Ticket subscribe(Callback callback) {
+        std::lock_guard<std::mutex> guard(mMutex);
+        while (true) {
+            const Ticket::value_t ticket = generateTicket();
+            const auto result = mSubscriptions.insert({ticket, {}});
+            if (result.second) {
+                result.first->second = std::move(callback);
+                return { .value = ticket };
+            }
+        }
+    }
 
-    void subscribe(void *object, NotifyFn notifyFn, Ticket *pTicket) {
-        return subscribeImpl(object, reinterpret_cast<void *>(notifyFn), pTicket);
+    void unsubscribe(const Ticket ticket) {
+        const auto value = ticket.value;
+        if (value) {
+            std::lock_guard<std::mutex> guard(mMutex);
+            mSubscriptions.erase(value);
+        }
+    }
+
+    void unsubscribe(Ticket *const ticket) {
+        unsubscribe(*ticket);
+        ticket->value = Ticket::kEmpty;
+    }
+
+    TopicBaseTpl(const TopicBaseTpl &) = delete;
+    TopicBaseTpl(TopicBaseTpl &&) = delete;
+    TopicBaseTpl& operator=(const TopicBaseTpl &) = delete;
+    TopicBaseTpl& operator=(TopicBaseTpl &&) = delete;
+
+protected:
+    TopicBaseTpl() = default;
+
+    Ticket::value_t generateTicket() {
+        const Ticket::value_t ticket = ++mLastTicket;
+        if (ticket != Ticket::kEmpty) {
+            return ticket;
+        } else {
+            return ++mLastTicket;
+        }
+    }
+
+    std::unordered_map<Ticket::value_t, Callback> mSubscriptions;
+    Ticket::value_t mLastTicket = Ticket::kEmpty;
+    std::mutex mMutex;
+};
+
+template <class... Args> struct Topic : public TopicBaseTpl<std::function<bool(Args...)>> {
+    using Callback = std::function<bool(Args...)>;
+    using TopicBase = TopicBaseTpl<Callback>;
+    using TopicBase::subscribe;
+    using TopicBase::mSubscriptions;
+    using TopicBase::mMutex;
+
+    template <class T> Ticket subscribe(const std::shared_ptr<T> &object,
+                                        bool (T::*const method)(Args...)) {
+        auto weakObject = std::weak_ptr<T>(object);
+
+        return subscribe([method, weakObject = std::move(weakObject)](Args... args){
+            const auto object = weakObject.lock();
+            return object && (*object.*method)(std::forward<Args>(args)...);
+        });
     }
 
     void broadcast(Args... args) {
         std::lock_guard<std::mutex> guard(mMutex);
-        size_t i = 0;
-        size_t size = mSubscriptions.size();
-
-        while (i < size) {
-            Subscription &si = mSubscriptions[i];
-            if ((*reinterpret_cast<NotifyFn>(si.notifyFn))(
-                        si.object, std::forward<Args>(args)...)) {
+        auto i = mSubscriptions.begin();
+        while (i != mSubscriptions.end()) {
+            if ((i->second)(std::forward<Args>(args)...)) {
                 ++i;
             } else {
-                unsubscribeImpl(i, size);
-                --size;
+                i = mSubscriptions.erase(i);
             }
         }
-
-        mSubscriptions.resize(size);
     }
 };
 
-template <> struct TopicT<void> : public Topic {
-    using NotifyFn = bool (*)(void *);
+template <> struct Topic<void> : public TopicBaseTpl<std::function<bool()>> {
+    using Callback = std::function<bool()>;
+    using TopicBase = TopicBaseTpl<Callback>;
+    using TopicBase::subscribe;
+    using TopicBase::mSubscriptions;
+    using TopicBase::mMutex;
 
-    void subscribe(void *object, NotifyFn notifyFn, Ticket *pTicket) {
-        return subscribeImpl(object, reinterpret_cast<void *>(notifyFn), pTicket);
+    template <class T> Ticket subscribe(const std::shared_ptr<T> &object,
+                                        bool (T::*const method)()) {
+        auto weakObject = std::weak_ptr<T>(object);
+
+        return subscribe([method, weakObject = std::move(weakObject)](){
+            const auto object = weakObject.lock();
+            return object && (*object.*method)();
+        });
     }
 
     void broadcast() {
         std::lock_guard<std::mutex> guard(mMutex);
-        size_t i = 0;
-        size_t size = mSubscriptions.size();
-
-        while (i < size) {
-            Subscription &si = mSubscriptions[i];
-            if ((*reinterpret_cast<NotifyFn>(si.notifyFn))(si.object)) {
+        auto i = mSubscriptions.begin();
+        while (i != mSubscriptions.end()) {
+            if ((i->second)()) {
                 ++i;
             } else {
-                unsubscribeImpl(i, size);
-                --size;
+                i = mSubscriptions.erase(i);
             }
         }
-
-        mSubscriptions.resize(size);
     }
 };
 
