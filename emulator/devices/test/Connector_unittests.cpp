16f955957df961f211d79a71848cada45d01d28b
Roman Kiryanov <rkir@google.com>
2024-07-24 23:30:57 +0000

    Fix a typo
    Bug: 353756694
    Test: presubmit
    Change-Id: I291c1a1eecfb4a24541c8e821b64b619aa813b08
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- emulator/devices/test/Connector_unittests.cpp
+++ emulator/devices/test/Connector_unittests.cpp
@@ -1,4 +1,4 @@
-// Copyright 2021 The Android Open Source Project
+// Copyright 2024 The Android Open Source Project
 //
 // This software is licensed under the terms of the GNU General Public
 // License version 2, as published by the Free Software Foundation, and
