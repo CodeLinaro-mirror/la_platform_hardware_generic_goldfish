254274d973f29465495dd2d4b45f83a1f27d2c85
Erwin Jansen <jansene@google.com>
2024-09-05 08:00:27 -0700

    Add qemu-looper support
    A qemu looper is a looper that is implemented on top of qemu. This can
    be used by qemu devices that need to make use of the android async looper infrastructure.
    
    Bug: 364869860
    Change-Id: Id94af1438ee6f0fd47cfde3c34bccc6132e81223

---
+++ emulator/plugin/avd/include/android/goldfish/qemu-looper.h
@@ -0,0 +1,27 @@
+// Copyright 2024 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+#include "aemu/base/async/Looper.h"
+
+namespace android {
+namespace goldfish {
+
+// An implementation of android::base::Looper on top of the QEMU main
+// event loop. There are few important things here:
+//
+//  1/ There is a single global QEMU event loop
+//
+//
+//  2/ It is not possible to call the runWithDeadlineMs() method, since
+//     the event loop is started in the application's main thread by
+//     the executable.
+android::base::Looper* qemuLooper();
+}  // namespace goldfish
+}  // namespace android
