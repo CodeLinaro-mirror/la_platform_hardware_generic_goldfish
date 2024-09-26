ce4704c26135a909e89532ec449f9f44fe9acfa3
Erwin Jansen <jansene@google.com>
2024-09-26 09:32:22 -0700

    Move adb host to its own package
    This moves the adb host functionality to its own independent package and
    updates the documentation to be doxygen compliant.
    
    Change-Id: Ief9e4ef4f46c6e43882a12d15c2d49073366ed53

---
+++ emulator/adb/host/include/android/emulation/control/adb/AdbHostServer.h
@@ -0,0 +1,52 @@
+// Copyright 2016 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#pragma once
+#include <optional>
+
+namespace android {
+namespace emulation {
+
+/**
+ * @brief Utility class for interacting with the host ADB Server.
+ */
+struct AdbHostServer {
+    /**
+     * @brief Notify the host server that an emulator instance is listening for connections.
+     *
+     * @param adbEmulatorPort The localhost TCP port the emulator is listening on.
+     * @param adbClientPort The localhost TCP port the ADB server is listening on.
+     * @return True on success, false on error.
+     */
+    static bool notify(int adbEmulatorPort, int adbClientPort = getClientPort());
+
+    /**
+     * @brief Default ADB client port value.
+     */
+    static constexpr int kDefaultAdbClientPort = 5037;
+
+    /**
+     * @brief Get the ADB client port to use for this emulator instance.
+     *
+     * @return The ADB client port.
+     */
+    static int getClientPort();
+
+    /**
+     * @brief Get the ADB protocol version reported by the running daemon.
+     *
+     * @return The ADB protocol version, or std::nullopt if unavailable
+     */
+    static std::optional<int> getProtocolVersion();
+};
+
+}  // namespace emulation
+}  // namespace android
