def151e45d537eaad768305110197e18580434a0
Erwin Jansen <jansene@google.com>
2024-11-13 10:06:26 -0800

    Introduce abslStatusToGrpcStatus for Abseil-gRPC interoperability
    The abslStatusToGrpcStatus function maps common absl::StatusCode values
    to their corresponding grpc::StatusCode equivalents. When no direct
    mapping exists, the function defaults to grpc::StatusCode::UNKNOWN,
    while always preserving the original error message from the
    absl::Status. This ensures consistent error handling across different
    libraries and simplifies the integration of gRPC services with code that
    leverages Abseil statuses.
    
    Change-Id: I618fd2f7d788a074d1e7852255ec33570b924d0a

---
+++ emulator/android-grpc/absl/include/android/grpc/utils/AbslStatusTranslate.h
@@ -0,0 +1,36 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+#include <grpcpp/support/status.h>
+
+#include "absl/status/status.h"
+
+namespace android::emulation::control {
+/**
+ * @brief Translates an absl::Status to a grpc::Status.
+ *
+ * This function maps an `absl::Status` object to its corresponding
+ * `grpc::Status` representation. This allows for interoperability between
+ * code using the Abseil status library and gRPC.  The function maps common
+ * `absl::StatusCode` values to their `grpc::StatusCode` equivalents. If no
+ * direct mapping exists, the function defaults to
+ * `grpc::StatusCode::UNKNOWN`. The error message from the absl::Status is
+ * always preserved.
+ *
+ * @param absl_status The input `absl::Status` object to be translated.
+ * @return The equivalent `grpc::Status` object. If the input status is OK,
+ *         returns a grpc::Status::OK.
+ */
+grpc::Status abslStatusToGrpcStatus(const absl::Status& absl_status);
+}  // namespace android::emulation::control
---
+++ emulator/android-grpc/absl/src/AbslStatusTranslate.cpp
@@ -0,0 +1,76 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "android/grpc/utils/AbslStatusTranslate.h"
+
+namespace android::emulation::control {
+grpc::Status abslStatusToGrpcStatus(const absl::Status& absl_status) {
+    if (absl_status.ok()) {
+        return grpc::Status::OK;
+    }
+
+    grpc::StatusCode grpc_code = grpc::StatusCode::UNKNOWN;  // Default
+
+    // Map some common absl::StatusCodes to grpc::StatusCodes.  Expand as needed.
+    switch (absl_status.code()) {
+        case absl::StatusCode::kCancelled:
+            grpc_code = grpc::StatusCode::CANCELLED;
+            break;
+        case absl::StatusCode::kInvalidArgument:
+            grpc_code = grpc::StatusCode::INVALID_ARGUMENT;
+            break;
+        case absl::StatusCode::kDeadlineExceeded:
+            grpc_code = grpc::StatusCode::DEADLINE_EXCEEDED;
+            break;
+        case absl::StatusCode::kNotFound:
+            grpc_code = grpc::StatusCode::NOT_FOUND;
+            break;
+        case absl::StatusCode::kAlreadyExists:
+            grpc_code = grpc::StatusCode::ALREADY_EXISTS;
+            break;
+        case absl::StatusCode::kPermissionDenied:
+            grpc_code = grpc::StatusCode::PERMISSION_DENIED;
+            break;
+        case absl::StatusCode::kResourceExhausted:
+            grpc_code = grpc::StatusCode::RESOURCE_EXHAUSTED;
+            break;
+        case absl::StatusCode::kFailedPrecondition:
+            grpc_code = grpc::StatusCode::FAILED_PRECONDITION;
+            break;
+        case absl::StatusCode::kAborted:
+            grpc_code = grpc::StatusCode::ABORTED;
+            break;
+        case absl::StatusCode::kOutOfRange:
+            grpc_code = grpc::StatusCode::OUT_OF_RANGE;
+            break;
+        case absl::StatusCode::kUnimplemented:
+            grpc_code = grpc::StatusCode::UNIMPLEMENTED;
+            break;
+        case absl::StatusCode::kInternal:
+            grpc_code = grpc::StatusCode::INTERNAL;
+            break;
+        case absl::StatusCode::kUnavailable:
+            grpc_code = grpc::StatusCode::UNAVAILABLE;
+            break;
+        case absl::StatusCode::kDataLoss:
+            grpc_code = grpc::StatusCode::DATA_LOSS;
+            break;
+        case absl::StatusCode::kUnauthenticated:
+            grpc_code = grpc::StatusCode::UNAUTHENTICATED;
+            break;
+            // Add more mappings as needed...
+    }
+
+    return ::grpc::Status(grpc_code, std::string(absl_status.message()));
+}
+}  // namespace android::emulation::control
