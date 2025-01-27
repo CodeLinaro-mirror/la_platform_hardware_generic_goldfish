048bf696af896def5d058863af80d9ddb9467022
Erwin Jansen <jansene@google.com>
2025-01-27 13:32:49 -0800

    feat: Add gtest_main target for Bazel file resolution
    This commit introduces a `gtest_main` target specifically designed for
    Google Tests that require dependencies on Bazel file resolution. This
    new target should be used instead of the standard `gtest_main` when
    tests need access to files specified in the 'data' section of the BUILD
    file.  This enables tests to correctly locate and utilize necessary data
    files within the Bazel environment.
    
    Change-Id: I51f11370ba6c559f684657f2efc4c0f028f1e9d7

---
+++ android/system/test/android/base/gtest_main.cpp
@@ -0,0 +1,31 @@
+// Copyright 2015 The Android Open Source Project
+//
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+#include "absl/log/globals.h"
+#include "absl/log/initialize.h"
+#include "absl/log/log.h"
+#include "gtest/gtest.h"
+
+#include "android/base/bazel/bazel_info.h"
+
+using android::base::Bazel;
+
+int main(int argc, char* argv[]) {
+    if (Bazel::inBazel()) {
+        Bazel::storeCommandLineArgs(argc, argv);
+    }
+
+    absl::InitializeLog();
+    absl::SetStderrThreshold(absl::LogSeverityAtLeast::kInfo);
+    absl::SetMinLogLevel(absl::LogSeverityAtLeast::kInfo);
+
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
