c6f1f2d8aacb2fc1292484ecf54b3dd236280238
Erwin Jansen <jansene@google.com>
2025-03-26 00:11:21 -0700

    refactor: Use PathEq matcher in config tests
    This commit refactors the config tests to use the `PathEq` matcher from
    `aemu/base/testing/FileMatchers.h` for comparing file paths. This
    improves the readability and maintainability of the tests.
    
    Change-Id: I0fece8c3a332c3f512345fee52ebedbb0863029f

--- emulator/config/test/android/goldfish/config/config_dirs_unittest.cpp
+++ emulator/config/test/android/goldfish/config/config_dirs_unittest.cpp
@@ -11,45 +11,46 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #include "android/goldfish/config/config_dirs.h"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "aemu/base/testing/FileMatchers.h"
 #include "android/base/testing/TestSystem.h"
 
 using android::base::TestSystem;
 using android::goldfish::ConfigDirs;
+using ::testing::Not;
+using ::testing::PathEq;
 
 namespace fs = std::filesystem;
 
 TEST(ConfigDirs, getUserDirectoryDefault) {
     TestSystem sys("bin", "myhome");
     fs::path kExpected = fs::path("myhome") / ".android";
-    EXPECT_EQ(kExpected, ConfigDirs::getUserDirectory());
+    EXPECT_THAT(ConfigDirs::getUserDirectory(), PathEq(kExpected));
 }
 
 TEST(ConfigDirs, getUserDirectoryWithAndroidSdkHome) {
     TestSystem sys("bin", "myhome");
     sys.envSet("ANDROID_SDK_HOME", "android-sdk");
-    EXPECT_STREQ("android-sdk", ConfigDirs::getUserDirectory().c_str());
+    EXPECT_THAT(ConfigDirs::getUserDirectory(), PathEq("android-sdk"));
 
     sys.getTempRoot()->makeSubDir(fs::path("android-sdk"));
     sys.getTempRoot()->makeSubDir(fs::path("android-sdk") / ".android");
-    EXPECT_STREQ((fs::path("android-sdk") / ".android").c_str(),
-                 ConfigDirs::getUserDirectory().c_str());
+    EXPECT_THAT(ConfigDirs::getUserDirectory(), PathEq("android-sdk/.android"));
 }
 
 TEST(ConfigDirs, getUserDirectoryWithAndroidSdkHomeAndPrefsRoot) {
     TestSystem sys("bin", "myhome");
     sys.envSet("ANDROID_SDK_HOME", "android-sdk");
     sys.envSet("ANDROID_SDK_HOME", "android-sdk-new");
-    EXPECT_STREQ("android-sdk-new", ConfigDirs::getUserDirectory().c_str());
+    EXPECT_THAT(ConfigDirs::getUserDirectory(), PathEq("android-sdk-new"));
 
     sys.getTempRoot()->makeSubDir(fs::path("android-sdk-new"));
     sys.getTempRoot()->makeSubDir(fs::path("android-sdk-new") / ".android");
-    EXPECT_STREQ((fs::path("android-sdk-new") / ".android").c_str(),
-                 ConfigDirs::getUserDirectory().c_str());
+    EXPECT_THAT(ConfigDirs::getUserDirectory(), PathEq("android-sdk-new/.android"));
 }
 
 TEST(ConfigDirs, getUserDirectoryWithAndroidEmulatorHome) {
@@ -57,10 +58,7 @@ TEST(ConfigDirs, getUserDirectoryWithAndroidEmulatorHome) {
     sys.envSet("ANDROID_EMULATOR_HOME",
                "android"
                "home");
-    EXPECT_STREQ(
-            "android"
-            "home",
-            ConfigDirs::getUserDirectory().c_str());
+    EXPECT_THAT(ConfigDirs::getUserDirectory(), PathEq("androidhome"));
 }
 
 TEST(ConfigDirs, getSdkRootDirectory) {
@@ -71,15 +69,15 @@ TEST(ConfigDirs, getSdkRootDirectory) {
     ASSERT_TRUE(sys.pathIsDir("Sdk"));
 
     sys.envSet("ANDROID_SDK_ROOT", "Sdk");
-    EXPECT_STREQ("Sdk", ConfigDirs::getSdkRootDirectory(true).c_str());
+    EXPECT_THAT(ConfigDirs::getSdkRootDirectory(true), PathEq("Sdk"));
 
     sys.envSet("ANDROID_SDK_ROOT",
                "\""
                "Sdk\"");
-    EXPECT_STREQ("Sdk", ConfigDirs::getSdkRootDirectory(true).c_str());
+    EXPECT_THAT(ConfigDirs::getSdkRootDirectory(true), PathEq("Sdk"));
 
     sys.envSet("ANDROID_SDK_ROOT", "");
-    EXPECT_STRNE("Sdk", ConfigDirs::getSdkRootDirectory(true).c_str());
+    EXPECT_THAT(ConfigDirs::getSdkRootDirectory(true), Not(PathEq("Sdk")));
 
     ASSERT_TRUE(sys.getTempRoot()->makeSubDir(fs::path("Sdk2")));
     ASSERT_TRUE(sys.getTempRoot()->makeSubDir(fs::path("Sdk2") / "platform-tools"));
@@ -88,12 +86,12 @@ TEST(ConfigDirs, getSdkRootDirectory) {
 
     // ANDROID_HOME should take precedence over ANDROID_SDK_ROOT
     sys.envSet("ANDROID_HOME", "Sdk2");
-    EXPECT_STREQ("Sdk2", ConfigDirs::getSdkRootDirectory(true).c_str());
+    EXPECT_THAT(ConfigDirs::getSdkRootDirectory(true), PathEq("Sdk2"));
 
     // Bad ANDROID_HOME falls back to ANDROID_SDK_ROOT
     sys.envSet("ANDROID_HOME", "bogus");
     sys.envSet("ANDROID_SDK_ROOT", "Sdk");
-    EXPECT_STREQ("Sdk", ConfigDirs::getSdkRootDirectory(true).c_str());
+    EXPECT_THAT(ConfigDirs::getSdkRootDirectory(true), PathEq("Sdk"));
 }
 
 TEST(ConfigDirs, getAvdRootDirectory) {
@@ -116,12 +114,12 @@ TEST(ConfigDirs, getAvdRootDirectory) {
     ASSERT_TRUE(sys.getTempRoot()->makeSubDir(fs::path("Area_6")));
     ASSERT_TRUE(sys.getTempRoot()->makeSubDir(fs::path("Area_6") / ".android"));
     ASSERT_TRUE(sys.getTempRoot()->makeSubDir(fs::path("Area_6") / ".android" / "avd"));
-    ASSERT_TRUE(sys.pathIsDir(fs::path("Area_1") / ".android" / "avd"));
-    ASSERT_TRUE(sys.pathIsDir(fs::path("Area_2") / ".android" / "avd"));
-    ASSERT_TRUE(sys.pathIsDir(fs::path("Area_3") / ".android" / "avd"));
-    ASSERT_TRUE(sys.pathIsDir(fs::path("Area_4") / ".android" / "avd"));
-    ASSERT_TRUE(sys.pathIsDir(fs::path("Area_5") / ".android" / "avd"));
-    ASSERT_TRUE(sys.pathIsDir(fs::path("Area_6") / ".android" / "avd"));
+    ASSERT_TRUE(sys.pathIsDir("Area_1/.android/avd"));
+    ASSERT_TRUE(sys.pathIsDir("Area_2/.android/avd"));
+    ASSERT_TRUE(sys.pathIsDir("Area_3/.android/avd"));
+    ASSERT_TRUE(sys.pathIsDir("Area_4/.android/avd"));
+    ASSERT_TRUE(sys.pathIsDir("Area_5/.android/avd"));
+    ASSERT_TRUE(sys.pathIsDir("Area_6/.android/avd"));
 
     // Order of precedence is
     //   ANDROID_AVD_HOME
@@ -131,32 +129,26 @@ TEST(ConfigDirs, getAvdRootDirectory) {
     //   USER_HOME or HOME
     //   ANDROID_EMULATOR_HOME
 
-    sys.envSet("ANDROID_AVD_HOME", (std::string)(fs::path("Area_1") / ".android" / "avd"));
-    sys.envSet("ANDROID_PREFS_ROOT", (std::string)fs::path("Area_2"));
-    sys.envSet("ANDROID_SDK_HOME", (std::string)fs::path("Area_3"));
-    sys.envSet("TEST_TMPDIR", (std::string)fs::path("Area_4"));
-    sys.envSet("USER_HOME", (std::string)fs::path("Area_5"));
-    sys.envSet("ANDROID_EMULATOR_HOME", (std::string)(fs::path("Area_6") / ".android"));
-    EXPECT_EQ((std::string)(fs::path("Area_1") / ".android" / "avd"),
-              ConfigDirs::getAvdRootDirectory().c_str());
+    sys.envSet("ANDROID_AVD_HOME", "Area_1/.android/avd");
+    sys.envSet("ANDROID_PREFS_ROOT", "Area_2");
+    sys.envSet("ANDROID_SDK_HOME", "Area_3");
+    sys.envSet("TEST_TMPDIR", "Area_4");
+    sys.envSet("USER_HOME", "Area_5");
+    sys.envSet("ANDROID_EMULATOR_HOME", "Area_6/.android");
+    EXPECT_THAT(ConfigDirs::getAvdRootDirectory(), PathEq("Area_1/.android/avd"));
 
     sys.envSet("ANDROID_AVD_HOME", "bogus");
-    EXPECT_EQ((fs::path("Area_2") / ".android" / "avd").string(),
-              ConfigDirs::getAvdRootDirectory().string());
+    EXPECT_THAT(ConfigDirs::getAvdRootDirectory(), PathEq("Area_2/.android/avd"));
 
     sys.envSet("ANDROID_PREFS_ROOT", "bogus");
-    EXPECT_EQ((std::string)(fs::path("Area_3") / ".android" / "avd"),
-              ConfigDirs::getAvdRootDirectory());
+    EXPECT_THAT(ConfigDirs::getAvdRootDirectory(), PathEq("Area_3/.android/avd"));
 
     sys.envSet("ANDROID_SDK_HOME", "bogus");
-    EXPECT_EQ((std::string)(fs::path("Area_4") / ".android" / "avd"),
-              ConfigDirs::getAvdRootDirectory().c_str());
+    EXPECT_THAT(ConfigDirs::getAvdRootDirectory(), PathEq("Area_4/.android/avd"));
 
     sys.envSet("TEST_TMPDIR", "bogus");
-    EXPECT_EQ((std::string)(fs::path("Area_5") / ".android" / "avd"),
-              ConfigDirs::getAvdRootDirectory().c_str());
+    EXPECT_THAT(ConfigDirs::getAvdRootDirectory(), PathEq("Area_5/.android/avd"));
 
     sys.envSet("USER_HOME", "bogus");
-    EXPECT_EQ((std::string)(fs::path("Area_6") / ".android" / "avd"),
-              ConfigDirs::getAvdRootDirectory().c_str());
+    EXPECT_THAT(ConfigDirs::getAvdRootDirectory(), PathEq("Area_6/.android/avd"));
 }
