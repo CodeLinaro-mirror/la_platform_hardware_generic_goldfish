488ac42605f27a49921c6237862e9a21fad64c21
Erwin Jansen <jansene@google.com>
2025-07-17 00:20:40 +0200

    fix: Fallback to program directory for launcher directory
    If the `ANDROID_EMULATOR_LAUNCHER_DIR` environment variable is not set,
    the system now correctly falls back to using the program's directory as
    the launcher directory. Previously, an empty `mLauncherDir` could lead
    to an incorrect or empty launcher directory being used.
    
    The `mLauncherDir` member variable has been removed as it is no longer
    needed to store the launcher directory persistently, as the value is now
    always derived dynamically.
    
    This change also includes new unit tests to ensure the correct behavior
    when the environment variable is present and when it's empty.
    
    Bug: 432033514
    Change-Id: I29bbc5b5c0c64215162e14e74baba58a15529fee

--- android/system/src/android/base/system/System.cpp
+++ android/system/src/android/base/system/System.cpp
@@ -302,16 +302,16 @@ class HostSystem : public System {
     }
 
     const fs::path getLauncherDirectory() const override {
-        if (mLauncherDir.empty()) {
-            std::string launcherDirEnv = envGet("ANDROID_EMULATOR_LAUNCHER_DIR");
-            if (!launcherDirEnv.empty()) {
-                mLauncherDir = std::move(launcherDirEnv);
-                VLOG(1) << "Using launcher dir from ANDROID_EMULATOR_LAUNCHER_DIR environment "
-                           "variable";
-            }
+        std::string launcherDirEnv = envGet("ANDROID_EMULATOR_LAUNCHER_DIR");
+        if (!launcherDirEnv.empty()) {
+            VLOG(1) << "Using launcher dir from ANDROID_EMULATOR_LAUNCHER_DIR environment "
+                       "variable: "
+                    << launcherDirEnv;
+            return launcherDirEnv;
         }
-        VLOG(1) << "Derived launcher directory: " << mLauncherDir;
-        return mLauncherDir;
+        launcherDirEnv = getProgramDirectory();
+        VLOG(1) << "Derived launcher directory: " << launcherDirEnv;
+        return launcherDirEnv;
     }
 
     const fs::path getHomeDirectory() const override {
@@ -1046,7 +1046,6 @@ class HostSystem : public System {
     static void atexit_HostSystem();
 
     mutable fs::path mProgramDir;
-    mutable fs::path mLauncherDir;
     mutable fs::path mHomeDir;
     mutable fs::path mAppDataDir;
 };
