ed202a1b6673158e8cfaf359f45059cf2175daa7
Roman Kiryanov <rkir@google.com>
2025-08-13 21:15:18 +0000

    virtualscene: implement it as a rotating triangle on Vulkan
    we will add our Torn1BD later.
    
    Bug: 404347465
    Change-Id: I88e73a87a8b1508970f2b218704964fcd109da27
    Signed-off-by: Roman Kiryanov <rkir@google.com>

---
+++ emulator/libs/gvk/include/goldfish/gvk/DeviceResources.h
@@ -0,0 +1,98 @@
+/* Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include "goldfish/base/UniqueHandle.h"
+
+namespace goldfish::gvk {
+
+struct DeviceDispatch;
+
+struct DeviceResourceDeleter {
+  struct Empty {};
+
+  explicit DeviceResourceDeleter(const DeviceDispatch* dd) : mDeviceDispatch(dd) {}
+  DeviceResourceDeleter(Empty) : mDeviceDispatch(nullptr) {}
+
+  void operator()(VkBuffer) const;
+  void operator()(VkCommandPool) const;
+  void operator()(VkDeviceMemory) const;
+  void operator()(VkDescriptorPool) const;
+  void operator()(VkDescriptorSetLayout) const;
+  void operator()(VkFramebuffer) const;
+  void operator()(VkImage) const;
+  void operator()(VkImageView) const;
+  void operator()(VkPipeline) const;
+  void operator()(VkPipelineLayout) const;
+  void operator()(VkRenderPass) const;
+  void operator()(VkShaderModule) const;
+
+ private:
+  const DeviceDispatch* mDeviceDispatch;
+};
+
+struct CommandBufferEnderImpl {
+  struct Empty {};
+
+  explicit CommandBufferEnderImpl(PFN_vkEndCommandBuffer endCommandBuffer);
+  CommandBufferEnderImpl(Empty) {}
+
+  void operator()(VkCommandBuffer) const;
+
+ private:
+  PFN_vkEndCommandBuffer mEndCommandBuffer = nullptr;
+};
+
+struct RenderPassEnderImpl {
+  struct Empty {};
+
+  explicit RenderPassEnderImpl(PFN_vkCmdEndRenderPass endRenderPass);
+  RenderPassEnderImpl(Empty) {}
+
+  void operator()(VkCommandBuffer) const;
+
+ private:
+  PFN_vkCmdEndRenderPass mEndRenderPass = nullptr;
+};
+
+using Buffer = goldfish::base::UniqueHandle<VkBuffer, VK_NULL_HANDLE, DeviceResourceDeleter>;
+using CommandPool =
+    goldfish::base::UniqueHandle<VkCommandPool, VK_NULL_HANDLE, DeviceResourceDeleter>;
+using CommandBufferEnder =
+    goldfish::base::UniqueHandle<VkCommandBuffer, VK_NULL_HANDLE, CommandBufferEnderImpl>;
+using DeviceMemory =
+    goldfish::base::UniqueHandle<VkDeviceMemory, VK_NULL_HANDLE, DeviceResourceDeleter>;
+using DescriptorPool =
+    goldfish::base::UniqueHandle<VkDescriptorPool, VK_NULL_HANDLE, DeviceResourceDeleter>;
+using DescriptorSetLayout =
+    goldfish::base::UniqueHandle<VkDescriptorSetLayout, VK_NULL_HANDLE, DeviceResourceDeleter>;
+using Framebuffer =
+    goldfish::base::UniqueHandle<VkFramebuffer, VK_NULL_HANDLE, DeviceResourceDeleter>;
+using Image = goldfish::base::UniqueHandle<VkImage, VK_NULL_HANDLE, DeviceResourceDeleter>;
+using ImageView = goldfish::base::UniqueHandle<VkImageView, VK_NULL_HANDLE, DeviceResourceDeleter>;
+using Pipeline = goldfish::base::UniqueHandle<VkPipeline, VK_NULL_HANDLE, DeviceResourceDeleter>;
+using PipelineLayout =
+    goldfish::base::UniqueHandle<VkPipelineLayout, VK_NULL_HANDLE, DeviceResourceDeleter>;
+using RenderPass =
+    goldfish::base::UniqueHandle<VkRenderPass, VK_NULL_HANDLE, DeviceResourceDeleter>;
+using RenderPassEnder =
+    goldfish::base::UniqueHandle<VkCommandBuffer, VK_NULL_HANDLE, RenderPassEnderImpl>;
+using ShaderModule =
+    goldfish::base::UniqueHandle<VkShaderModule, VK_NULL_HANDLE, DeviceResourceDeleter>;
+
+}  // namespace goldfish::gvk
---
+++ emulator/libs/imaging/toVkFormat.cpp
@@ -0,0 +1,35 @@
+/* Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "goldfish/imaging/toVkFormat.h"
+
+namespace goldfish::imaging {
+
+VkFormat toVkFormat(const ImageFormat fmt) {
+    switch (fmt) {
+    case ImageFormat::NONE:
+        break;
+    case ImageFormat::RGBA_8888:
+        return VK_FORMAT_R8G8B8A8_UNORM;
+    case ImageFormat::YUV420_3P:
+        return VK_FORMAT_G8_B8_R8_3PLANE_420_UNORM;
+    case ImageFormat::YUV420_NV12:
+        return VK_FORMAT_G8_B8R8_2PLANE_420_UNORM;
+    }
+
+    return VK_FORMAT_UNDEFINED;
+}
+
+}  // namespace goldfish::imaging
