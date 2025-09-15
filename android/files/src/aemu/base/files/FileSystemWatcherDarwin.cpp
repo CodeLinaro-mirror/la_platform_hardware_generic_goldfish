39eb90b0672120fadd20bdec58819ee637115e2a
Erwin Jansen <jansene@google.com>
2025-09-15 14:06:05 -0700

    fix(base): Make FileSystemWatcher reliable on macOS
    The previous `FSEvents`-based implementation (`FileSystemWatcherFS`) was
    unreliable. Although it correctly requested per-file events using
    `kFSEventStreamCreateFlagFileEvents`, the callback (`watcherCb`)
    ignored this per-file data.
    
    Instead, the callback triggered a full, manual directory rescan
    (`scanAndNotify`) on *every* event. This `scanAndNotify` mechanism was
    slow, racy, and frequently missed events, especially rapid modifications
    and deletions.
    
    This change updates `watcherCb` to process the individual file paths
    and event flags provided directly by the FSEvents API. This allows it
    to correctly and reliably dispatch `Created`, `Deleted`, and `Changed`
    events without resorting to a manual directory diff.
    
    Additionally, this patch:
    - Adds a new unit test suite (`FileSystemWatcher_unittest.cpp`) to
      verify the corrected behavior and prevent regressions.
    - Removes the old, unused `FileSystemWatcherKQueue` implementation.
    - Corrects the file license on `FileSystemWatcherDarwin.cpp` to ASL 2.0.
    - Includes minor loop and FD initialization fixes for the POSIX watcher.
    
    Change-Id: I991d7dda587743ec0b9714534826bacbf9cbfc03

--- android/files/src/aemu/base/files/FileSystemWatcherDarwin.cpp
+++ android/files/src/aemu/base/files/FileSystemWatcherDarwin.cpp
@@ -1,23 +1,18 @@
-// Copyright 2022 The Android Open Source Project
+// Copyright (C) 2025 The Android Open Source Project
 //
-// This software is licensed under the terms of the GNU General Public
-// License version 2, as published by the Free Software Foundation, and
-// may be copied, distributed, and modified under those terms.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #include <CoreServices/CoreServices.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <inttypes.h>
-#include <limits.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/event.h>
-#include <sys/stat.h>
-#include <unistd.h>
 
 #include <atomic>
 #include <chrono>
@@ -26,266 +21,123 @@
 #include <memory>
 #include <string>
 #include <thread>
-#include <unordered_map>
-#include <utility>
-#include <vector>
 
 #include "absl/log/log.h"
+#include "absl/strings/string_view.h"
 
 #include "aemu/base/files/FileSystemWatcher.h"
 #include "aemu/base/synchronization/Event.h"
 #include "android/base/system/System.h"
 
-#define DEBUG 0
-
-#if DEBUG >= 1
-#define DD(fmt, ...) \
-    printf("FileSystemWatcherKQueue: %s:%d| " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
-#else
-#define DD(...) (void)0
-#endif
-
 namespace android {
 namespace base {
 
-using Path = FileSystemWatcher::Path;
-// struct PathHash {
-//     std::size_t operator()(Path const& p) const noexcept {
-//         return Path::hash_value(p);
-//     }
-// };
-
-// Maps the path to the actual file stat.
-using PathMap = std::unordered_map<Path, struct stat>;
-
-static PathMap scanDir(Path scanPath) {
-    PathMap paths;
-    for (auto strPath : System::get()->scanDirEntries(scanPath.c_str(), true)) {
-        struct stat fstat;
-        stat(strPath.c_str(), &fstat);
-        paths[strPath] = fstat;
-        DD("Added %s (%d)", strPath.c_str(), fstat.st_size);
-    }
-    return paths;
-}
-
-bool statEqual(struct stat lhs, struct stat rhs) {
-    return lhs.st_dev == rhs.st_dev && lhs.st_ino == rhs.st_ino && lhs.st_mode == rhs.st_mode &&
-           lhs.st_nlink == rhs.st_nlink && lhs.st_uid == rhs.st_uid && lhs.st_gid == rhs.st_gid &&
-           lhs.st_rdev == rhs.st_rdev && lhs.st_size == rhs.st_size &&
-           lhs.st_flags == rhs.st_flags && lhs.st_mtimespec.tv_sec == rhs.st_mtimespec.tv_sec &&
-           lhs.st_mtimespec.tv_nsec == rhs.st_mtimespec.tv_nsec;
-}
-
-// Scan's the given path, invoking the callback if the current scan state
-// results in a mismatch.
-static PathMap scanAndNotify(Path scanPath, const PathMap& oldState,
-                             FileSystemWatcher::FileSystemWatcherCallback callback) {
-    // dir add/remove
-    auto currentState = scanDir(scanPath);
-    for (const auto& [name, state] : oldState) {
-        if (currentState.count(name) == 0) {
-            // name got deleted.
-            callback(FileSystemWatcher::WatcherChangeType::Deleted, name);
-        } else if (!statEqual(currentState[name], state)) {
-            // name changed
-            DD("Name change due to stat mismatch for %s", name.c_str());
-            callback(FileSystemWatcher::WatcherChangeType::Changed, name);
-        }
-    }
-    for (auto& [name, state] : currentState) {
-        if (oldState.count(name) == 0) {
-            // The creation of a file or folder.
-            callback(FileSystemWatcher::WatcherChangeType::Created, name);
-        }
-    }
-
-    return currentState;
-}
-
-// File system events based upon kernel queues
-// https://developer.apple.com/library/archive/documentation/Darwin/Conceptual/FSEvents_ProgGuide/KernelQueues/KernelQueues.html
-class FileSystemWatcherKQueue : public FileSystemWatcher {
-  public:
-    FileSystemWatcherKQueue(Path path, FileSystemWatcherCallback onChangeCallback)
-        : FileSystemWatcher(onChangeCallback), mPath(path), mDirectoryView(scanDir(path)) {}
-
-    ~FileSystemWatcherKQueue() { stop(); }
-
-    bool start() override {
-        bool expected = false;
-        if (!mRunning.compare_exchange_strong(expected, true)) {
-            return false;
-        }
-        std::thread watcher([this] { watchForChanges(); });
-        mWatcherThread = std::move(watcher);
-        mStarted.wait();
-        return mPipe[0] != -1;
-    }
-
-    void stop() override {
-        bool expected = true;
-        if (mRunning.compare_exchange_strong(expected, false)) {
-            DD("Stopping thread.");
-            write(mPipe[1], "x", 1);
-            DD("Done");
-            mWatcherThread.join();
-        }
-    }
-
-  private:
-    bool watchForChanges() {
-        constexpr int NUM_EVENT_SLOTS = 1;
-        constexpr int NUM_EVENT_FDS = 2;
-        int kq;
-        int event_fd;
-        struct kevent events_to_monitor[NUM_EVENT_FDS];
-        struct kevent event_data[NUM_EVENT_SLOTS];
-        void* user_data;
-        unsigned int vnode_events;
-
-        if ((kq = kqueue()) < 0) {
-            PLOG(ERROR) << "Unable to watch for changes.";
-            mStarted.signal();
-            return false;
-        }
-
-        if (pipe(mPipe) != 0 || (fcntl(mPipe[0], F_SETFL, O_NONBLOCK) < 0)) {
-            PLOG(ERROR) << "Unable to open pipe.";
-            mPipe[1] = -1;
-            mStarted.signal();
-            return false;
-        };
-
-        DD("Pipes: %d <-> %d", mPipe[0], mPipe[1]);
-
-        event_fd = open(mPath.c_str(), O_EVTONLY);
-        if (event_fd <= 0) {
-            LOG(ERROR) << "The file " << mPath << " could not be opened for monitoring. Error was "
-                       << strerror(errno);
-            close(mPipe[0]);
-            close(mPipe[1]);
-            mPipe[1] = -1;
-            mStarted.signal();
-            return false;
-        }
-
-        // Pipe is used to signal exit.
-        EV_SET(events_to_monitor, mPipe[0], EVFILT_READ, EV_ADD, 0, 0, NULL);
-
-        vnode_events = NOTE_DELETE | NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB | NOTE_LINK |
-                       NOTE_RENAME | NOTE_REVOKE;
-        EV_SET(events_to_monitor + 1, event_fd, EVFILT_VNODE, EV_ADD | EV_ENABLE | EV_ONESHOT,
-               vnode_events, 0, nullptr);
-
-        /* Handle events. */
-        int num_files = 1;
-        mStarted.signal();
-        while (mRunning) {
-            int event_count =
-                    kevent(kq, events_to_monitor, NUM_EVENT_FDS, event_data, num_files, 0);
-            DD("Got %d events\n", event_count);
-            if (mRunning) {
-                if ((event_count < 0) || (event_data[0].flags == EV_ERROR)) {
-                    /* An error occurred. */
-                    LOG(ERROR) << "An error occurred (event count " << event_count
-                               << "). The error was " << strerror(errno) << ".";
-                    break;
-                }
-                if (event_count) {
-                    DD("fd: %d, Event %" PRIdPTR
-                       " occurred.  Filter %d, flags %d, filter flags %s, "
-                       "filter data %" PRIdPTR ", path %s\n",
-                       event_fd, event_data[0].ident, event_data[0].filter, event_data[0].flags,
-                       flagstring(event_data[0].fflags), event_data[0].data,
-                       (char*)event_data[0].udata);
-                    // Okay, let's interpret the event
-                    handleChange(event_fd, event_data);
-                }
-            }
-        }
-
-        close(event_fd);
-        close(mPipe[0]);
-        close(mPipe[1]);
-        return true;
-    }
-
-    /* A simple routine to return a string for a set of flags. */
-    char* flagstring(int flags) {
-        static char ret[512];
-        char* orr = "";
-
-        ret[0] = '\0';  // clear the string.
-        if (flags & NOTE_DELETE) {
-            strcat(ret, orr);
-            strcat(ret, "NOTE_DELETE");
-            orr = "|";
-        }
-        if (flags & NOTE_WRITE) {
-            strcat(ret, orr);
-            strcat(ret, "NOTE_WRITE");
-            orr = "|";
-        }
-        if (flags & NOTE_EXTEND) {
-            strcat(ret, orr);
-            strcat(ret, "NOTE_EXTEND");
-            orr = "|";
-        }
-        if (flags & NOTE_ATTRIB) {
-            strcat(ret, orr);
-            strcat(ret, "NOTE_ATTRIB");
-            orr = "|";
-        }
-        if (flags & NOTE_LINK) {
-            strcat(ret, orr);
-            strcat(ret, "NOTE_LINK");
-            orr = "|";
-        }
-        if (flags & NOTE_RENAME) {
-            strcat(ret, orr);
-            strcat(ret, "NOTE_RENAME");
-            orr = "|";
-        }
-        if (flags & NOTE_REVOKE) {
-            strcat(ret, orr);
-            strcat(ret, "NOTE_REVOKE");
-            orr = "|";
-        }
-        return ret;
-    }
-
-    void handleChange(int fd, struct kevent* event_data) {
-        int flags = event_data->fflags;
-        if (flags & NOTE_DELETE) {
-            mChangeCallback(WatcherChangeType::Deleted, mPath);
-        } else if (flags & NOTE_ATTRIB) {
-            mChangeCallback(WatcherChangeType::Changed, mPath);
-        } else if (flags & NOTE_RENAME) {
-            char newPath[PATH_MAX] = {0};
-            fcntl(fd, F_GETPATH, newPath);
-            mChangeCallback(WatcherChangeType::Deleted, mPath);
-            mChangeCallback(WatcherChangeType::Created, newPath);
-        } else {
-            mDirectoryView = std::move(scanAndNotify(mPath, mDirectoryView, mChangeCallback));
-        }
-    }
-
-    Path mPath;
-    PathMap mDirectoryView{};
-    std::atomic_bool mRunning{false};
-    std::thread mWatcherThread;
-    Event mStarted;
-    int mPipe[2];
+namespace {
+// Debug helpers, this allows us to log the file flags in a human readable form.
+struct FSEventFlagsWrapper {
+    FSEventStreamEventFlags flags;
 };
+
+template <typename Sink>
+void AppendFlag(Sink& sink, bool* first, absl::string_view flag) {
+    if (!(*first)) {
+        sink.Append("|");
+    }
+    sink.Append(flag);
+    *first = false;
+}
+
+template <typename Sink>
+void AbslStringify(Sink& sink, FSEventFlagsWrapper wrapper) {
+    FSEventStreamEventFlags flags = wrapper.flags;
+    if (flags == kFSEventStreamEventFlagNone) {
+        sink.Append("None");
+        return;
+    }
+
+    bool first = true;
+
+    // --- Item-level flags ---
+    if (flags & kFSEventStreamEventFlagItemCreated) {
+        AppendFlag(sink, &first, "Created");
+    }
+    if (flags & kFSEventStreamEventFlagItemRemoved) {
+        AppendFlag(sink, &first, "Removed");
+    }
+    if (flags & kFSEventStreamEventFlagItemModified) {
+        AppendFlag(sink, &first, "Modified");
+    }
+    if (flags & kFSEventStreamEventFlagItemInodeMetaMod) {
+        AppendFlag(sink, &first, "InodeMetaMod");
+    }
+    if (flags & kFSEventStreamEventFlagItemRenamed) {
+        AppendFlag(sink, &first, "Renamed");
+    }
+    if (flags & kFSEventStreamEventFlagItemXattrMod) {
+        AppendFlag(sink, &first, "XattrMod");
+    }
+    if (flags & kFSEventStreamEventFlagItemFinderInfoMod) {
+        AppendFlag(sink, &first, "FinderInfoMod");
+    }
+    if (flags & kFSEventStreamEventFlagItemChangeOwner) {
+        AppendFlag(sink, &first, "ChangeOwner");
+    }
+    if (flags & kFSEventStreamEventFlagItemCloned) {
+        AppendFlag(sink, &first, "Cloned");
+    }
+
+    // --- Item type flags ---
+    if (flags & kFSEventStreamEventFlagItemIsFile) {
+        AppendFlag(sink, &first, "IsFile");
+    }
+    if (flags & kFSEventStreamEventFlagItemIsDir) {
+        AppendFlag(sink, &first, "IsDir");
+    }
+    if (flags & kFSEventStreamEventFlagItemIsSymlink) {
+        AppendFlag(sink, &first, "IsSymlink");
+    }
+    if (flags & kFSEventStreamEventFlagItemIsHardlink) {
+        AppendFlag(sink, &first, "IsHardlink");
+    }
+
+    // --- Stream-level flags ---
+    if (flags & kFSEventStreamEventFlagMustScanSubDirs) {
+        AppendFlag(sink, &first, "MustScanSubDirs");
+    }
+    if (flags & kFSEventStreamEventFlagUserDropped) {
+        AppendFlag(sink, &first, "UserDropped");
+    }
+    if (flags & kFSEventStreamEventFlagKernelDropped) {
+        AppendFlag(sink, &first, "KernelDropped");
+    }
+    if (flags & kFSEventStreamEventFlagEventIdsWrapped) {
+        AppendFlag(sink, &first, "EventIdsWrapped");
+    }
+    if (flags & kFSEventStreamEventFlagHistoryDone) {
+        AppendFlag(sink, &first, "HistoryDone");
+    }
+    if (flags & kFSEventStreamEventFlagRootChanged) {
+        AppendFlag(sink, &first, "RootChanged");
+    }
+    if (flags & kFSEventStreamEventFlagMount) {
+        AppendFlag(sink, &first, "Mount");
+    }
+    if (flags & kFSEventStreamEventFlagUnmount) {
+        AppendFlag(sink, &first, "Unmount");
+    }
+}
+}  // namespace
+using Path = FileSystemWatcher::Path;
 
 // Filesystem watcher based on
 // https://developer.apple.com/documentation/coreservices/file_system_events
 // api.
+//
+// Note: Modify events *only* detect timestamp and attribute changes.
 class FileSystemWatcherFS : public FileSystemWatcher {
   public:
     FileSystemWatcherFS(Path path, FileSystemWatcherCallback onChangeCallback)
-        : FileSystemWatcher(onChangeCallback), mPath(path), mDirectoryView(scanDir(path)) {}
+            : FileSystemWatcher(onChangeCallback), mPath(path) {}
 
     ~FileSystemWatcherFS() { stop(); }
 
@@ -303,8 +155,11 @@ class FileSystemWatcherFS : public FileSystemWatcher {
     void stop() override {
         bool expected = true;
         if (mRunning.compare_exchange_strong(expected, false)) {
-            DD("Stopping loop.");
-            CFRunLoopStop(mCfRunLoop);
+            VLOG(1) << "Stopping loop.";
+            if (mCfRunLoop) {
+                CFRunLoopStop(mCfRunLoop);
+            }
+            mStarted.signal();
             mWatcherThread.join();
         }
     }
@@ -313,15 +168,28 @@ class FileSystemWatcherFS : public FileSystemWatcher {
     static void watcherCb(ConstFSEventStreamRef, void* clientCallBackInfo, size_t numEvents,
                           void* eventPaths, const FSEventStreamEventFlags eventFlags[],
                           const FSEventStreamEventId*) {
-        // Sadly the event does not include the actual filepath, but only the
-        // root path. so, best we can do is rescan the structure and see what
-        // has changed.
-        // See:
-        // https://developer.apple.com/documentation/coreservices/fseventstreamcallback
-
         auto* watcher = static_cast<FileSystemWatcherFS*>(clientCallBackInfo);
-        watcher->mDirectoryView = std::move(
-                scanAndNotify(watcher->mPath, watcher->mDirectoryView, watcher->mChangeCallback));
+        char** paths = (char**)eventPaths;
+
+        for (size_t i = 0; i < numEvents; i++) {
+            std::string path = paths[i];
+            const FSEventStreamEventFlags flags = eventFlags[i];
+
+            VLOG(1) << "FSEvent: path=" << path << ", flags=" << FSEventFlagsWrapper{flags};
+
+            if (flags & kFSEventStreamEventFlagItemRemoved) {
+                watcher->mChangeCallback(WatcherChangeType::Deleted, path);
+            } else if (flags & kFSEventStreamEventFlagItemModified &&
+                       (flags & kFSEventStreamEventFlagItemInodeMetaMod ||
+                        flags & kFSEventStreamEventFlagItemXattrMod)) {
+                // Note, most change events are fired as Created|Modified|...
+                // we only take the ones that modify the attr, and inode meta.
+                watcher->mChangeCallback(WatcherChangeType::Changed, path);
+            } else if (flags & kFSEventStreamEventFlagItemCreated) {
+                // You might get a change event (i.e. file contents change) as a create event.
+                watcher->mChangeCallback(WatcherChangeType::Created, path);
+            }
+        }
     }
 
     bool watchForChanges() {
@@ -331,26 +199,27 @@ class FileSystemWatcherFS : public FileSystemWatcher {
                                           &kCFTypeArrayCallBacks);
 
         FSEventStreamContext streamCtx = {0, this, NULL, NULL, NULL};
-        auto stream = FSEventStreamCreate(
-                nullptr, &FileSystemWatcherFS::watcherCb, &streamCtx, pathsToWatch,
-                kFSEventStreamEventIdSinceNow, 0,
-                kFSEventStreamCreateFlagFileEvents | kFSEventStreamCreateFlagNoDefer);
+        auto stream =
+                FSEventStreamCreate(nullptr, &FileSystemWatcherFS::watcherCb, &streamCtx,
+                                    pathsToWatch, kFSEventStreamEventIdSinceNow, 0,
+                                    kFSEventStreamCreateFlagFileEvents |  // Get file-level events
+                                            kFSEventStreamCreateFlagNoDefer);  // Get them ASAP
 
         if (!stream) {
             mStarted.signal();
             return false;
         }
 
-        // Fire the event loop
         mCfRunLoop = CFRunLoopGetCurrent();
         FSEventStreamScheduleWithRunLoop(stream, mCfRunLoop, kCFRunLoopDefaultMode);
         FSEventStreamStart(stream);
 
-        DD("Starting run loop.");
         mStarted.signal();
-        CFRunLoopRun();
 
-        DD("Done..");
+        VLOG(1) << "Starting run loop.";
+        CFRunLoopRun();  // Waits until we cancel it (by calling CFRunLoopStop).
+
+        VLOG(1) << "Completed watcher loop.";
         FSEventStreamStop(stream);
         FSEventStreamInvalidate(stream);
         FSEventStreamRelease(stream);
@@ -362,8 +231,6 @@ class FileSystemWatcherFS : public FileSystemWatcher {
     std::atomic_bool mRunning{false};
     std::thread mWatcherThread;
     Event mStarted;
-    PathMap mDirectoryView{};
-
     CFRunLoopRef mCfRunLoop;
 };
 
