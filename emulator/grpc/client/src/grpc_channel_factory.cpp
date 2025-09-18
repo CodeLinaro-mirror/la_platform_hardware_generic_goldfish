28bcc69450228561f18fb6d8a44c2603ed458691
Erwin Jansen <jansene@google.com>
2025-09-18 08:37:39 -0700

    feat(grpc): Add new blocking and async gRPC clients
    This commit rewrites the gRPC client used to connect to the emulator.
    The previous implementation was a single class and has been replaced
    with a more robust, testable, and flexible architecture.
    
    The new implementation provides two distinct client types built via
    `EmulatorGrpcClientBuilder`:
    
    * `BlockingEmulatorGrpcClient`: A simple, synchronous client for
      straightforward scripting or tasks.
    
    * `CallbackEmulatorGrpcClient`: An advanced, asynchronous client
      designed for long-lived applications (like UIs). It manages a
      background thread to provide:
      - Non-blocking `connectAsync()`
      - Automatic liveness monitoring to detect server disconnects.
      - A `connectionStateChanges` event source for reactive state updates.
    
    This wil be used by the ui to track the state of the remote endpoint and
    to establish connections without blocking the ui.
    
    Bug: 331192780
    Change-Id: I5295e889c846c15112bc18fb2561309de49aeb92

---
+++ emulator/grpc/client/src/grpc_channel_factory.cpp
@@ -0,0 +1,126 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "grpc_channel_factory.h"
+
+#include <grpcpp/grpcpp.h>
+
+#include <fstream>
+#include <string>
+#include <unordered_map>
+
+#include "absl/log/log.h"
+#include "absl/strings/match.h"
+#include "absl/strings/str_cat.h"
+
+namespace android {
+namespace emulation {
+namespace control {
+
+namespace {
+// A plugin that inserts a set of headers.
+class HeaderInjector : public grpc::MetadataCredentialsPlugin {
+  public:
+    explicit HeaderInjector(std::unordered_map<std::string, std::string> headers)
+            : mHeaders(std::move(headers)) {}
+    ~HeaderInjector() override = default;
+
+    grpc::Status GetMetadata(grpc::string_ref service_url, grpc::string_ref method_name,
+                             const grpc::AuthContext& channel_auth_context,
+                             std::multimap<grpc::string, grpc::string>* metadata) override {
+        for (const auto& v : mHeaders) {
+            metadata->insert(std::make_pair(v.first, v.second));
+        }
+        return grpc::Status::OK;
+    }
+
+  private:
+    std::unordered_map<std::string, std::string> mHeaders;
+};
+
+static absl::StatusOr<std::string> readFile(const std::filesystem::path& fname) {
+    std::ifstream fstream(fname);
+    if (!fstream) {
+        return absl::NotFoundError(absl::StrCat("File not found: ", fname.string()));
+    }
+    std::string contents((std::istreambuf_iterator<char>(fstream)),
+                         std::istreambuf_iterator<char>());
+    return contents;
+}
+}  // namespace
+
+GrpcChannelFactory::GrpcChannelFactory(const Endpoint& endpoint, InterceptorFactories interceptors)
+        : mEndpoint(endpoint), mInterceptors(std::move(interceptors)) {}
+
+std::shared_ptr<grpc::CallCredentials> GrpcChannelFactory::credentials() const {
+    if (mCredentials) {
+        return mCredentials;
+    }
+
+    if (!mEndpoint.required_headers().empty()) {
+        std::unordered_map<std::string, std::string> map;
+        for (const auto& header : mEndpoint.required_headers()) {
+            map[header.key()] = header.value();
+        }
+        mCredentials = grpc::MetadataCredentialsFromPlugin(
+                std::make_unique<HeaderInjector>(std::move(map)));
+    }
+    return mCredentials;
+}
+
+std::shared_ptr<grpc::Channel> GrpcChannelFactory::createChannel() {
+    auto address = mEndpoint.target();
+    std::shared_ptr<grpc::ChannelCredentials> channel_creds;
+
+    std::string ca_path = mEndpoint.tls_credentials().pem_root_certs();
+    std::string key_path = mEndpoint.tls_credentials().pem_private_key();
+    std::string cer_path = mEndpoint.tls_credentials().pem_cert_chain();
+
+    if (!ca_path.empty() && !key_path.empty() && !cer_path.empty()) {
+        grpc::SslCredentialsOptions sslOpts;
+        auto ca = readFile(ca_path);
+        auto key = readFile(key_path);
+        auto cer = readFile(cer_path);
+
+        if (ca.ok() && key.ok() && cer.ok()) {
+            sslOpts.pem_root_certs = *ca;
+            sslOpts.pem_private_key = *key;
+            sslOpts.pem_cert_chain = *cer;
+            channel_creds = grpc::SslCredentials(sslOpts);
+        } else {
+            LOG(ERROR) << "Failed to read TLS credentials.";
+            return nullptr;
+        }
+    } else if (absl::StartsWith(address, "127.0.0.1") || absl::StartsWith(address, "localhost")) {
+        channel_creds = ::grpc::experimental::LocalCredentials(LOCAL_TCP);
+    } else {
+        LOG(ERROR) << "Insecure connections are not supported for non-local "
+                      "addresses. Please configure TLS credentials.";
+        return nullptr;
+    }
+
+    auto creds = credentials();
+    if (creds) {
+        channel_creds = grpc::CompositeChannelCredentials(channel_creds, creds);
+    }
+
+    grpc::ChannelArguments maxSize;
+    maxSize.SetMaxReceiveMessageSize(-1);
+    return grpc::experimental::CreateCustomChannelWithInterceptors(address, channel_creds, maxSize,
+                                                                   std::move(mInterceptors));
+}
+
+}  // namespace control
+}  // namespace emulation
+}  // namespace android
