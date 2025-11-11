21d32eb51d088bff1bf026baeb261f4ab304b621
Roman Kiryanov <rkir@google.com>
2025-11-11 00:04:05 +0000

    Use vec3 and vec4 from glm
    we already bring glm to sensors
    
    Change-Id: I53118e2d49dd00e93bd2bc7e9276fe19085032cf
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- emulator/hal/sensors/include/goldfish/devices/sensor/Sensors.h
+++ emulator/hal/sensors/include/goldfish/devices/sensor/Sensors.h
@@ -14,48 +14,5 @@
 
 #pragma once
 
-#include <math.h>
-
 #include "goldfish/devices/sensor/AndroidSensor.h"
 #include "goldfish/devices/sensor/PhysicalParameter.h"
-
-namespace goldfish::devices::sensor {
-
-/* struct for use in sensor values */
-struct vec3 {
-    float x;
-    float y;
-    float z;
-    bool operator==(const vec3& rhs) const {
-        const float kEpsilon = 0.00001f;
-
-        const double diffX = fabs(x - rhs.x);
-        const double diffY = fabs(y - rhs.y);
-        const double diffZ = fabs(z - rhs.z);
-        return (diffX < kEpsilon && diffY < kEpsilon && diffZ < kEpsilon);
-    }
-
-    bool operator!=(const vec3& rhs) const { return !(*this == rhs); }
-};
-
-/* struct for use in sensor values */
-struct vec4 {
-    float x;
-    float y;
-    float z;
-    float w;
-
-    bool operator==(const vec4& rhs) const {
-        const float kEpsilon = 0.00001f;
-
-        const double diffX = fabs(x - rhs.x);
-        const double diffY = fabs(y - rhs.y);
-        const double diffZ = fabs(z - rhs.z);
-        const double diffW = fabs(w - rhs.w);
-        return (diffX < kEpsilon && diffY < kEpsilon && diffZ < kEpsilon && diffW < kEpsilon);
-    }
-
-    bool operator!=(const vec4& rhs) const { return !(*this == rhs); }
-};
-
-}  // namespace goldfish::devices::sensor
