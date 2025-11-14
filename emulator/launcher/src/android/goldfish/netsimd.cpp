057b3b2bb4a2f6a1eed007c5a9764ec3fdb4cbfb
Will Hollins <whollins@google.com>
2025-11-14 15:56:13 +0000

    Don't disable netsim cli connections when launching netsimd
    Change-Id: I4f5ecf86dba7290d45bcf572c01f81a9744bd3c7

--- emulator/launcher/src/android/goldfish/netsimd.cpp
+++ emulator/launcher/src/android/goldfish/netsimd.cpp
@@ -84,7 +84,7 @@ absl::StatusOr<NetsimConnection_ptr> connect_to_netsim(const std::string& endpoi
 
 absl::StatusOr<::goldfish::async::LaunchConfig> netsimd_launch_config(
         const std::filesystem::path& netsim_binary, const AndroidOptions& opts) {
-    bool no_cli_ui = true;  //! feature_is_enabled(kFeature_NetsimCliUi),
+    bool no_cli_ui = false;  //! feature_is_enabled(kFeature_NetsimCliUi),
     bool no_web_ui = true;  //! feature_is_enabled(kFeature_NetsimWebUi),
     std::string host_dns = opts.dns_server ? opts.dns_server : "";
     if (host_dns.empty()) {
