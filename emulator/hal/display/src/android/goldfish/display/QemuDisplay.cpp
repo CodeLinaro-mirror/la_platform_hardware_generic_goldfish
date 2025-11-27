769eaeeebf2a34363ff791e09df054cc4d1578ca
Will Hollins <whollins@google.com>
2025-11-27 21:18:37 +0000

    Move very noisy VLOGs to level 2
    Change-Id: I6a520c9443d05542b85770c72d58bbf4db977d63

--- emulator/hal/display/src/android/goldfish/display/QemuDisplay.cpp
+++ emulator/hal/display/src/android/goldfish/display/QemuDisplay.cpp
@@ -93,7 +93,7 @@ static uint32_t bmap[INPUT_BUTTON__MAX] = {
 
 void QemuDisplay::sendMouseEvent(int x, int y, int button_mask) {
     absl::MutexLock lock(&mSendLock);
-    VLOG(1) << *this << ", sendMouseEvent(" << x << ", " << y << ", " << button_mask << ")";
+    VLOG(2) << *this << ", sendMouseEvent(" << x << ", " << y << ", " << button_mask << ")";
     (void)mQemuLoop->post([con = mConsole, x, y, w = mWidth, h = mHeight, last = mlast_bmask,
                      mask = button_mask] {
         if (last != mask) {
