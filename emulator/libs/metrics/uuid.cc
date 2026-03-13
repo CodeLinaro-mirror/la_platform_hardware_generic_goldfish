e11042b64f02ae471ca56a68f6afe4dba0323729
Will Hollins <whollins@google.com>
2026-03-13 16:44:19 +0000

    Add simple Uuid class
    This is a simplified port from emu-main-dev.
    
    Bug: 476380758
    Change-Id: I4c979652b8d4e2120746f0ed41091e41ddc37c67

---
+++ emulator/libs/metrics/uuid.cc
@@ -0,0 +1,80 @@
+// Copyright (C) 2026 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS);
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "goldfish/metrics/uuid.h"
+
+#include <string_view>
+
+#ifdef _WIN32
+#include <rpc.h>
+#else
+#include <uuid/uuid.h>
+#endif
+
+#include "absl/strings/str_cat.h"
+
+namespace goldfish::metrics {
+
+namespace {
+constexpr std::string_view kNullUuidStr = "00000000-0000-0000-0000-000000000000";
+}
+
+absl::StatusOr<Uuid> Uuid::FromString(std::string_view uuid_str) {
+    Uuid uuid;
+#ifdef _WIN32
+     if (::UuidFromStringA(reinterpret_cast<RPC_CSTR>(const_cast<char*>(uuid_str.data())), &uuid.data_) != RPC_S_OK) {
+        return absl::InvalidArgumentError(absl::StrCat("couldn't parse string as uuid: ", uuid_str));
+    }
+#else
+    if (uuid_parse_range(uuid_str.data(), uuid_str.data() + uuid_str.size(), uuid.data_.data()) < 0) {
+        return absl::InvalidArgumentError(absl::StrCat("couldn't parse string as uuid: ", uuid_str));
+    }
+#endif
+    return uuid;
+}
+
+Uuid Uuid::Generate() {
+    Uuid res;
+#ifdef _WIN32
+    ::UuidCreate(&res.data_);
+#else
+    uuid_generate(res.data_.data());
+#endif
+    return res;
+}
+
+Uuid Uuid::Zero() {
+    static const Uuid zero = *FromString(kNullUuidStr);
+    return zero;
+}
+
+std::string Uuid::ToString() const {
+#ifdef _WIN32
+    unsigned char* str = nullptr;
+    if (::UuidToStringA(&data_, &str) != RPC_S_OK || !str) {
+        return std::string(kNullUuidStr);
+    }
+
+    std::string res(reinterpret_cast<char*>(str));
+    ::RpcStringFree(&str);
+#else
+    std::string res(kNullUuidStr.size() + 1, '\0');
+    uuid_unparse_lower(data_.data(), &res[0]);
+    res.pop_back();
+#endif
+    return res;
+}
+
+}  // namespace goldfish::metrics
---
+++ emulator/libs/metrics/uuid_test.cc
@@ -0,0 +1,53 @@
+/* Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "goldfish/metrics/uuid.h"
+
+#include <gtest/gtest.h>
+
+#include "android/status/status_matcher_macros.h"
+
+namespace goldfish::metrics {
+
+TEST(UuidTest, Zero) {
+    Uuid uuid = Uuid::Zero();
+    EXPECT_EQ(uuid.ToString(), "00000000-0000-0000-0000-000000000000");
+}
+
+TEST(UuidTest, Generate) {
+    auto uuid1 = Uuid::Generate();
+    auto uuid2 = Uuid::Generate();
+    EXPECT_NE(uuid1, Uuid::Zero());
+    EXPECT_NE(uuid2, Uuid::Zero());
+    EXPECT_NE(uuid1, uuid2);
+}
+
+TEST(UuidTest, ParseValid) {
+    std::string s = "550e8400-e29b-41d4-a716-446655440000";
+    ASSERT_OK_AND_ASSIGN(auto uuid, Uuid::FromString(s));
+    EXPECT_EQ(uuid.ToString(), s);
+}
+
+TEST(UuidTest, ParseInvalid) {
+    EXPECT_THAT(Uuid::FromString("not-a-uuid"), ::absl_testing::StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(UuidTest, Equality) {
+    Uuid uuid1 = Uuid::Generate();
+    ASSERT_OK_AND_ASSIGN(auto uuid2, Uuid::FromString(uuid1.ToString()));
+    EXPECT_EQ(uuid1, uuid2);
+}
+
+} // namespace goldfish::metrics
