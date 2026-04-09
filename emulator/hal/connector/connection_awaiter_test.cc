b55fea56a81fbca1da1b95e3485b5a4f8ec4a80b
Roman Kiryanov <rkir@google.com>
2026-04-09 02:02:31 +0000

    Fix memory leaks in connection_awaiter_test
    ISocket instances must be managed by some kind
    of socket specific manager.
    
    Bug: 500850632
    Change-Id: Ia5dad31b6ec695025404a05bc09603e6bc05a488
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- emulator/hal/connector/connection_awaiter_test.cc
+++ emulator/hal/connector/connection_awaiter_test.cc
@@ -21,6 +21,7 @@
 #include <memory>
 #include <thread>
 
+#include "absl/log/check.h"
 #include "absl/log/log.h"
 
 #include "goldfish/async/testing/test_event_loop.h"
@@ -40,28 +41,48 @@ using namespace std::chrono_literals;
 
 class TestSocket : public ISocket {
   public:
-    TestSocket(PlugPtr p) : plug(std::move(p)) {}
+    TestSocket(const uint32_t id, PlugPtr p) : id_(id), plug_(std::move(p)) {}
     void SendAsync(const void* const data, const size_t size) override {
         const uint8_t* const data8 = static_cast<const uint8_t*>(data);
-        storage.insert(storage.end(), data8, data8 + size);
+        storage_.insert(storage_.end(), data8, data8 + size);
     }
 
     PlugPtr SwitchPlug(PlugPtr newPlug) override {
-        plug.swap(newPlug);
+        plug_.swap(newPlug);
         return newPlug;
     }
 
-    PlugPtr UnplugImpl() override {
-        PlugPtr p = std::move(this->plug);
-        delete this;
-        return p;
-    }
+    PlugPtr UnplugImpl() override { return std::move(plug_); }
 
-    void fakeConnected() { plug->OnConnect(); }
+    void fakeConnected() { plug_->OnConnect(); }
+
+    bool operator<(const TestSocket& rhs) const { return id_ < rhs.id_; }
 
   private:
-    std::vector<uint8_t> storage;
-    PlugPtr plug;
+    const uint32_t id_;
+    std::vector<uint8_t> storage_;
+    PlugPtr plug_;
+};
+
+class TestSocketManager {
+  public:
+    ~TestSocketManager() {
+        for (const TestSocket& ts : sockets_) {
+            if (const PlugPtr p = const_cast<TestSocket&>(ts).UnplugImpl()) {
+                p->OnUnplug();
+            }
+        }
+    }
+
+    SocketPtr Accept(PlugPtr p) {
+        const auto [where, inserted] = sockets_.emplace(++idGenerator_, std::move(p));
+        CHECK(inserted);
+        return SocketPtr(&const_cast<TestSocket&>(*where));
+    }
+
+  private:
+    std::set<TestSocket> sockets_;
+    uint32_t idGenerator_ = 0;
 };
 
 class NullPlug : public IPlug {
@@ -72,8 +93,8 @@ class NullPlug : public IPlug {
     SocketPtr OnUnplug() override { return nullptr; };
 };
 
-SocketPtr fakeConnection(async::EventLoop* event_loop, PlugPtr plug) {
-    auto ptr = SocketPtr(new TestSocket(plug));
+SocketPtr fakeConnection(async::EventLoop* event_loop, TestSocketManager& tsm, PlugPtr plug) {
+    auto ptr = tsm.Accept(std::move(plug));
     (void)event_loop->Post(
             [socket = ptr.get()]() { static_cast<TestSocket*>(socket)->fakeConnected(); });
     return ptr;
@@ -83,15 +104,20 @@ using namespace std::chrono_literals;
 
 TEST(ConnectionAwaiter, make_fake_connection) {
     auto event_loop = TestEventLoop::Create();
+    TestSocketManager tsm;
+
     auto plug = std::make_shared<NullPlug>();
-    auto socket = fakeConnection(event_loop.get(), plug);
+    auto socket = fakeConnection(event_loop.get(), tsm, plug);
 }
 
 TEST(ConnectionAwaiter, fires_on_connect) {
-    bool connected = false;
     auto event_loop = TestEventLoop::Create();
+    TestSocketManager tsm;
+
+    bool connected = false;
     auto ready = ConnectionAwaiter::RetryUntilConnected(
-            event_loop.get(), [&](auto plug) { return fakeConnection(event_loop.get(), plug); },
+            event_loop.get(),
+            [&](auto plug) { return fakeConnection(event_loop.get(), tsm, plug); },
             [&](SocketPtr sock) { connected = true; }, 10ms);
 
     event_loop->AdvanceClock(10ms);
@@ -100,15 +126,17 @@ TEST(ConnectionAwaiter, fires_on_connect) {
 }
 
 TEST(ConnectionAwaiter, tries_to_connect_multiple_times) {
+    auto event_loop = TestEventLoop::Create();
+    TestSocketManager tsm;
+
     bool connected = false;
     int invocation = 0;
-    auto event_loop = TestEventLoop::Create();
     auto ready = ConnectionAwaiter::RetryUntilConnected(
             event_loop.get(),
             [&](auto plug) {
                 invocation++;
                 VLOG(1) << "Connection attempt: " << invocation;
-                return SocketPtr(new TestSocket(plug));
+                return tsm.Accept(std::move(plug));
             },
             [&](SocketPtr sock) { connected = true; }, 10ms);
 
@@ -121,18 +149,20 @@ TEST(ConnectionAwaiter, tries_to_connect_multiple_times) {
 }
 
 TEST(ConnectionAwaiter, stop_calling_after_connect) {
+    auto event_loop = TestEventLoop::Create();
+    TestSocketManager tsm;
+
     bool connected = false;
     int invocation = 0;
-    auto event_loop = TestEventLoop::Create();
     auto ready = ConnectionAwaiter::RetryUntilConnected(
             event_loop.get(),
             [&](auto plug) {
                 // On the third invocation we will connect.
                 invocation++;
                 if (invocation == 3) {
-                    return fakeConnection(event_loop.get(), plug);
+                    return fakeConnection(event_loop.get(), tsm, std::move(plug));
                 }
-                return SocketPtr(new TestSocket(plug));
+                return tsm.Accept(std::move(plug));
             },
             [&](SocketPtr sock) { connected = true; }, 10ms);
 
