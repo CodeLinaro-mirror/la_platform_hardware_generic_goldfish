ba283df83b7011912b86e80ff31196ac9e99ee9c
Will Hollins <whollins@google.com>
2026-04-28 14:16:21 +0100

    Fix tsan and asan suppression file settings for unit tests
    Change-Id: Ieefa819e2c55065804365c57f5bcbd8fa234004d

--- emulator/libs/test/gtest_main.cc
+++ emulator/libs/test/gtest_main.cc
@@ -8,21 +8,49 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
+
+#include <vector>
+
 #include "absl/debugging/failure_signal_handler.h"
 #include "absl/debugging/symbolize.h"
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "absl/log/globals.h"
 #include "absl/log/initialize.h"
+#include "absl/strings/str_cat.h"
 #include "gtest/gtest.h"
 
 #include "android/base/bazel_info.h"
+#include "android/base/system.h"
 
 using android::base::Bazel;
+using android::base::System;
 
 ABSL_FLAG(bool, verbose_test, false,
           "Enable verbose test logging, equivalent to "
           "--vmodule=\"*=1\" --logtostderr=true --stderrthreshold=0.");
+
+namespace {
+
+void setup_sanitizers() {
+#ifdef __linux__
+    System::SetEnvironmentVariable("LLVM_SYMBOLIZER", Bazel::RunfilesPath("goldfish_build++toolchain+clang_linux_x64/bin/llvm-symbolizer"));
+#elifdef __APPLE__
+    System::SetEnvironmentVariable("LLVM_SYMBOLIZER", Bazel::RunfilesPath("goldfish_build++toolchain+clang_mac_all/bin/llvm-symbolizer"));
+#endif
+
+#if defined(__has_feature)
+#if __has_feature(address_sanitizer)
+    System::SetEnvironmentVariable("LSAN_OPTIONS", absl::StrCat("suppressions=", Bazel::RunfilesPath("goldfish+/emulator/libs/test/leak_suppressions.txt")));
+    System::SetEnvironmentVariable("ASAN_OPTIONS", "detect_odr_violation=0");
+#endif
+#if __has_feature(thread_sanitizer)
+    System::SetEnvironmentVariable("TSAN_OPTIONS", absl::StrCat("second_deadlock_stack=1,die_after_fork=0,suppressions=", Bazel::RunfilesPath("goldfish+/emulator/libs/test/tsan_suppressions.txt")));
+#endif
+#endif
+}
+
+} // namespace
 
 int main(int argc, char* argv[]) {
     absl::InitializeSymbolizer(argv[0]);
@@ -32,6 +60,8 @@ int main(int argc, char* argv[]) {
     if (Bazel::InBazel()) {
         Bazel::StoreCommandLineArgs(argc, argv);
     }
+
+    setup_sanitizers();
 
     // Parse abseil logging configuration.
     std::vector<char*> positional_args;
