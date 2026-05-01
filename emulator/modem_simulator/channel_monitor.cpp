66ec62e12e992ccabdee7350e7b5c3668064b8d9
Roman Kiryanov <rkir@google.com>
2026-05-01 19:16:22 +0000

    modem_simulator: protect ChannelMonitor::clients* with a mutex
    these fields are accessed from
    
    * monitor_thread_ (created in ChannelMonitor's ctor)
    * SetRemoteClient called from host_processor_
      (ModemSimulatorServiceImpl's ctor)
    
    Bug: 501490919
    Change-Id: Ifc4ace0271cb7740eca58d00b63829fdf467c529
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- emulator/modem_simulator/channel_monitor.cpp
+++ emulator/modem_simulator/channel_monitor.cpp
@@ -89,8 +89,7 @@ ChannelMonitor::ChannelMonitor(ModemSimulator& modem, SharedFD server)
   }
 }
 
-std::optional<ClientId> ChannelMonitor::SetRemoteClient(
-    SharedFD client, bool is_accepted) ABSL_NO_THREAD_SAFETY_ANALYSIS {
+std::optional<ClientId> ChannelMonitor::SetRemoteClient(SharedFD client, bool is_accepted) {
   auto remote_client = std::make_unique<Client>(client, Client::REMOTE);
 
   if (is_accepted) {
@@ -103,8 +102,12 @@ std::optional<ClientId> ChannelMonitor::SetRemoteClient(
 
   auto id = remote_client->Id();
   remote_client->first_read_command_ = false;
-  remote_clients_.push_back(std::move(remote_client));
-  VLOG(1) << "added one remote client";
+
+  {
+    const absl::MutexLock lock(remote_clients_mutex_);
+    remote_clients_.push_back(std::move(remote_client));
+    VLOG(1) << "added one remote client";
+  }
 
   // Trigger monitor loop
   if (write_pipe_->IsOpen()) {
@@ -116,13 +119,14 @@ std::optional<ClientId> ChannelMonitor::SetRemoteClient(
   return id;
 }
 
-void ChannelMonitor::AcceptIncomingConnection() ABSL_NO_THREAD_SAFETY_ANALYSIS {
+void ChannelMonitor::AcceptIncomingConnection() {
   auto client_fd = SharedFD::Accept(*server_);
   if (!client_fd->IsOpen()) {
     LOG(ERROR) << "Error accepting connection on socket: " << client_fd->StrError();
   } else {
     auto client = std::make_unique<Client>(client_fd);
     VLOG(1) << "added one RIL client";
+
     clients_.push_back(std::move(client));
     if (clients_.size() == 1) {
       // The first connected client default to be the unsolicited commands channel
@@ -131,7 +135,7 @@ void ChannelMonitor::AcceptIncomingConnection() ABSL_NO_THREAD_SAFETY_ANALYSIS {
   }
 }
 
-bool ChannelMonitor::ReadCommand(Client& client) ABSL_NO_THREAD_SAFETY_ANALYSIS {
+bool ChannelMonitor::ReadCommand(Client& client) {
   std::vector<char> buffer(kMaxCommandLength);
   auto bytes_read = client.client_read_fd_->Read(buffer.data(), buffer.size());
   if (bytes_read <= 0) {
@@ -180,46 +184,54 @@ bool ChannelMonitor::ReadCommand(Client& client) ABSL_NO_THREAD_SAFETY_ANALYSIS 
   return true;
 }
 
-void ChannelMonitor::SendUnsolicitedCommand(const std::string& response) ABSL_NO_THREAD_SAFETY_ANALYSIS {
-  // The first accepted client default to be unsolicited command channel?
-  auto iter = clients_.begin();
-  if (iter != clients_.end()) {
-    iter->get()->SendCommandResponse(response);
+void ChannelMonitor::SendUnsolicitedCommand(const std::string& response) {
+  if (!clients_.empty()) {
+    // The first accepted client default to be unsolicited command channel?
+    clients_.front()->SendCommandResponse(response);
   } else {
     VLOG(1) << "No client connected yet.";
   }
 }
 
-void ChannelMonitor::SendRemoteCommand(const ClientId client, const std::string& response) ABSL_NO_THREAD_SAFETY_ANALYSIS {
-  auto iter = remote_clients_.begin();
-  for (; iter != remote_clients_.end(); ++iter) {
-    if (iter->get()->Id() == client) {
-      iter->get()->SendCommandResponse(response);
-      return;
-    }
+void ChannelMonitor::SendRemoteCommand(const ClientId clientId, const std::string& response) {
+  const absl::MutexLock lock(remote_clients_mutex_);
+  const auto i = std::find_if(
+      remote_clients_.begin(), remote_clients_.end(),
+      [clientId](const std::unique_ptr<Client>& client) {
+          return client->Id() == clientId;
+      });
+
+  if (i != remote_clients_.end()) {
+    (*i)->SendCommandResponse(response);
+  } else {
+    VLOG(1) << "Remote client has closed.";
   }
-  VLOG(1) << "Remote client has closed.";
 }
 
-void ChannelMonitor::CloseRemoteConnection(ClientId clientId) {
-  auto iter = remote_clients_.begin();
-  for (; iter != remote_clients_.end(); ++iter) {
-    Client& client = **iter;
+void ChannelMonitor::CloseRemoteConnection(const ClientId clientId) {
+  {
+    const absl::MutexLock lock(remote_clients_mutex_);
+    const auto i = std::find_if(
+        remote_clients_.begin(), remote_clients_.end(),
+        [clientId](const std::unique_ptr<Client>& client) {
+            return client->Id() == clientId;
+        });
 
-    if (client.Id() == clientId) {
-      client.Close();
-
-      // Trigger monitor loop
-      if (write_pipe_->IsOpen()) {
-        write_pipe_->Write("OK", sizeof("OK"));
-        VLOG(1) << "asking to remove clients";
-      } else {
-        LOG(ERROR) << "Pipe created fail, can't trigger monitor loop";
-      }
+    if (i == remote_clients_.end()) {
+      VLOG(1) << "Remote client has been erased.";
       return;
     }
+
+    (*i)->Close();
   }
-  VLOG(1) << "Remote client has been erased.";
+
+  // Trigger monitor loop
+  if (write_pipe_->IsOpen()) {
+    write_pipe_->Write("OK", sizeof("OK"));
+    VLOG(1) << "asking to remove clients";
+  } else {
+    LOG(ERROR) << "Pipe created fail, can't trigger monitor loop";
+  }
 }
 
 ChannelMonitor::~ChannelMonitor() {
@@ -233,34 +245,27 @@ ChannelMonitor::~ChannelMonitor() {
   }
 }
 
-void ChannelMonitor::removeInvalidClients(
-    std::vector<std::unique_ptr<Client>>& clients) ABSL_NO_THREAD_SAFETY_ANALYSIS {
-  auto iter = clients.begin();
-  for (; iter != clients.end();) {
-    if (iter->get()->is_valid) {
-      ++iter;
-    } else {
-      VLOG(1) << "removed 1 client";
-      iter = clients.erase(iter);
-    }
-  }
-}
-
-void ChannelMonitor::MonitorLoop() ABSL_NO_THREAD_SAFETY_ANALYSIS {
+void ChannelMonitor::MonitorLoop() {
   do {
     cuttlefish::SharedFDSet read_set;
     read_set.Set(server_);
     read_set.Set(read_pipe_);
+
     for (auto& client : clients_) {
       if (client->is_valid) {
         read_set.Set(client->client_read_fd_);
       }
     }
-    for (auto& client : remote_clients_) {
-      if (client->is_valid) {
-        read_set.Set(client->client_read_fd_);
+
+    {
+      const absl::MutexLock lock(remote_clients_mutex_);
+      for (auto& client : remote_clients_) {
+        if (client->is_valid) {
+          read_set.Set(client->client_read_fd_);
+        }
       }
     }
+
     int num_fds = cuttlefish::Select(&read_set, nullptr, nullptr, nullptr);
     if (num_fds < 0) {
       LOG(ERROR) << "Select call returned error : " << strerror(errno);
@@ -277,9 +282,15 @@ void ChannelMonitor::MonitorLoop() ABSL_NO_THREAD_SAFETY_ANALYSIS {
           VLOG(1) << "requested to exit now";
           break;
         }
-        // clean the lists
-        removeInvalidClients(clients_);
-        removeInvalidClients(remote_clients_);
+
+        const auto is_invalid_client = [](const std::unique_ptr<Client>& client) {
+          return !client->is_valid;
+        };
+
+        std::erase_if(clients_, is_invalid_client);
+
+        const absl::MutexLock lock(remote_clients_mutex_);
+        std::erase_if(remote_clients_, is_invalid_client);
       }
 
       std::erase_if(clients_, [&read_set, this](const std::unique_ptr<Client>& client) {
@@ -290,6 +301,7 @@ void ChannelMonitor::MonitorLoop() ABSL_NO_THREAD_SAFETY_ANALYSIS {
         }
       });
 
+      const absl::MutexLock lock(remote_clients_mutex_);
       std::erase_if(remote_clients_, [&read_set, this](const std::unique_ptr<Client>& client) {
         if (read_set.IsSet(client->client_read_fd_)) {
           return !ReadCommand(*client);
