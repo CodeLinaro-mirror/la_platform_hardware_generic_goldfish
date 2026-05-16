d4733e5ef2ea2c74df3e30716733e2851f3914ad
Bo Hu <bohu@google.com>
2026-05-16 21:22:39 +0000

    add hugepage support on linux
    This is important for nested virt environment
    such as gcp to have better speed booting emulator
    could be 2-3x faster booting time
    
    Note: qemu will try to use huge pages even
    without this, but it may just try best effort
    and it often ends up using partially. this cl
    will be more aggressively get all it needs
    at the upfront, but still fallback if it cannot.
    
    Bug: 496647016
    
    Change-Id: Id52b0546d180eaa63756e9716d832055080afefa

--- emulator/launcher/launch_qemu/launch_qemu.cc
+++ emulator/launcher/launch_qemu/launch_qemu.cc
@@ -43,6 +43,7 @@
 #include "cpu_device.h"
 #include "display_device.h"
 #include "gpu_device.h"
+#include "hugepage_device.h"
 #include "initrd_device.h"
 #include "kernel_device.h"
 #include "machine.h"
@@ -90,6 +91,7 @@ absl::Status LaunchQemu::addDevices() {
     addDevice<Machine>();
     addDevice<CpuDevice>();
     addDevice<MemoryDevice>();
+    addDevice<HugePageDevice>();
     addDevice<KernelDevice>();
     addDevice<InitrdDevice>();
 
