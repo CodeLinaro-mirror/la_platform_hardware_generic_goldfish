afab3876ef0d73979107a9688270bccdb3fcd422
Serdar Kocdemir <kocdemir@google.com>
2026-07-03 14:05:13 +0100

    Limit available GPU memory and hostmem to 4GB
    Some memory tests will use the available memory size to randomly
    allocate memory. Since lavapipe only supports a single heap, this
    results in very large host visible memory allocations to be requested;
    the current limit 256M causes ENOSPC errors from the kernel side. Until
    we can create a second heap on the fly to limit host visible allocations
    in a different way, we set the limits to 4GB, as we need at least 3GB
    memory to run some of the common applications and 4GB is the recommended
    minimum ram for supported AVDs.
    
    Bug: 514638991
    Test: dEQP-VK.memory.mapping.suballocation.random*
    Change-Id: I6771ce8bfe9a9385021f56945aebe5ba130b0400

--- emulator/launcher/launch_qemu/gpu_device.cc
+++ emulator/launcher/launch_qemu/gpu_device.cc
@@ -60,6 +60,9 @@ std::vector<std::string> GpuDevice::getQemuParameters(const EmulatorConfig& emul
 
     renderer_features.append(";VulkanBatchedDescriptorSetUpdate:disabled");
 
+    // Limit available GPU memory and hostmem to 4GB
+    renderer_features.append(";VulkanMaxSafeHeapSize:4294967296");
+
     // Temporarily limit guest to Vulkan 1.3, unless 1.4 is explicitly enabled via an env variable.
     const char* env_vk_enable_1_4 = getenv("ANDROID_EMU_VK_ENABLE_1_4");
     if (!env_vk_enable_1_4 || env_vk_enable_1_4[0] == '0') {
@@ -78,7 +81,7 @@ std::vector<std::string> GpuDevice::getQemuParameters(const EmulatorConfig& emul
     }
 
     params.push_back(absl::StrCat("id=", mGpuName));
-    params.push_back("hostmem=256M");
+    params.push_back("hostmem=4096M");
     params.push_back(gfxstream_backends);
     params.push_back("x-gfxstream-composer=on");
     params.push_back(absl::StrCat("renderer_features=", renderer_features));
